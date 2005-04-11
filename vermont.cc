7bedc736bb97edbdeda2660d5b723016739a91db
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-11 15:14:50 +0000

    -Add configuration functions to main vermont.cc -Add configuration struct
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@79 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- vermont.cc
+++ vermont.cc
@@ -36,38 +36,41 @@
 
 using namespace std;
 
+/* holding all objects/handles/... for the subsystems like sampler and collector */
+struct v_objects {
+
+	/* main vermont: */
+        /* the configuration struct, from iniparser */
+	dictionary *v_config;
+	/* initialized subsystems */
+	unsigned int v_subsystems;
+
+	/* for sampler: */
+	Template *templ;
+	Observer *observer;
+        Filter *filter;
+	vector<PacketProcessor *> processors;
+	ExporterSink *exporter;
+		
+	/* for collector: */
+	int collector_handle;
+};
+
 static void usage();
 static void sig_handler(int x);
 static int setup_signal(int signal, void (*handler)(int));
-int vermont_readconf(char *file, dictionary **conf);
-static Template * configure_template(char *list);
-
-/* initialized subsystems */
-unsigned int v_subsystems;
-
-/* holding all objects/handles/... for the subsystems like sampler and collector */
-struct {
-
-	/* for sampler */
-	Observer *observer;
-	vector<PacketProcessor *> processors;
-        Filter *main_filter;
-	Template *templ;
-	ExporterSink *exporter;
-		
-	/* for collector */
-	//int collector_handle;
-	/* callbacks ... ? */
-
-} v_objects;
-
+static int vermont_readconf(dictionary **conf, char *file);
+static int vermont_configure(struct v_objects *v, dictionary *conf);
+static Template * configure_template(uint16_t template_id, char *list);
+static Observer * configure_observer(char *interface, int snaplen);
 
 int main(int ac, char **dc)
 {
-	dictionary *config;
+        dictionary *config;
 	int c, debug_level=MSG_DEFAULT;
         char *config_file=NULL;
-        Template *templ;
+
+        struct v_objects v_objects;
 
 	/* parse command line */
 	while((c=getopt(ac, dc, "hf:d")) != -1) {
@@ -97,18 +100,12 @@ int main(int ac, char **dc)
 
 	setup_signal(SIGINT, sig_handler);
 
-	if(vermont_readconf(config_file, &config)) {
+	if(vermont_readconf(&config, config_file) || vermont_configure(&v_objects, config)) {
 		exit(-1);
 	}
 
-        /* now configure the template */
-	if(!(templ=configure_template(iniparser_getvalue(config, "sampler", "template")))) {
-		msg(MSG_FATAL, "could not configure a template");
-                exit(1);
-        }
 
-        subsys_dump(v_subsystems);
-	iniparser_dump(config, stdout);
+        subsys_dump(v_objects.v_subsystems);
 
         return 0;
 }
@@ -117,8 +114,14 @@ int main(int ac, char **dc)
 /*
  read the config from *file and attach the iniparser stuff to **conf
  perform basic checks
+
+ NOTE: the funny thing is, all returned strings are direct pointer into
+ the dictionary-struct and as such valid until the dictionary is
+ free()'d again.
+
+ SO WE USUALLY DONT COPY!
  */
-int vermont_readconf(char *file, dictionary **conf)
+static int vermont_readconf(dictionary **conf, char *file)
 {
         dictionary *d;
 
@@ -133,21 +136,77 @@ int vermont_readconf(char *file, dictionary **conf)
 	   (iniparser_find_entry(d, "sampler") == 1) &&
 	   (iniparser_find_entry(d, "main") == 1)
 	  ) {
-		subsys_on(&v_subsystems, SUBSYS_CONFIG);
 		*conf=d;
 
 		return 0;
 
 	} else {
-		msg(MSG_FATAL, "not all needed sections in config %s", file);
+		msg(MSG_FATAL, "Config: not all needed sections in config %s", file);
 
 		return -1;
 	}
 }
 
 
+/*
+ this is the main configuration entry point
+ configure all subsystems peu a peu
+ */
+static int vermont_configure(struct v_objects *v, dictionary *conf)
+{
+	/* now configure the template */
+	if(!(v->templ=configure_template(
+					 atoi(iniparser_getvalue(conf, "sampler", "template_id")),
+					 iniparser_getvalue(conf, "sampler", "template")
+					)
+	    )) {
+		msg(MSG_FATAL, "Config: could not configure a template");
+		return(1);
+	}
+
+
+	/* FIXME: handle interface = off */
+	if(!(v->observer=configure_observer(
+					    iniparser_getvalue(conf, "sampler", "interface"),
+                                            atoi(iniparser_getvalue(conf, "sampler", "capturelen"))
+					   )
+	    )) {
+		msg(MSG_FATAL, "Config: could not configure an observer(pcap)");
+		return(1);
+	}
+
+
+	if(!(v->filter=configure_filter())) {
+
+
+	}
+
+
+
+	/* attach the successfully used config into the main config struct */
+	v->v_config=conf;
+
+	return 0;
+}
+
+
+/*
+ configure an observer, listening at interface with capturelen
+ capturelen 0 means use Observer's default
+ */
+static Observer * configure_observer(char *interface, int snaplen)
+{
+	Observer *o=new Observer(interface);
+
+	if(snaplen) {
+		if(! o->setCaptureLen(snaplen)) {
+			msg(MSG_FATAL, "Observer: wrong snaplen specified, using %d", o->getCaptureLen());
+		}
+	}
+}
+
 /* configure the sampler template from a "," separated list */
-static Template * configure_template(char *list)
+static Template * configure_template(uint16_t template_id, char *list)
 {
 	Template *t;
 	char *l, *token;
@@ -160,7 +219,9 @@ static Template * configure_template(char *list)
 	}
 	
         /* assemble the Template */
-	t=new Template(2);
+	msg(MSG_INFO, "Template: using ID %d", template_id);
+	t=new Template(template_id);
+
 	while((token=strsep(&l, ","))) {
 
 		/*
@@ -168,16 +229,23 @@ static Template * configure_template(char *list)
 		 name_lookup returns -1 on error, id_lookup NULL
                  make use of short circuit in C for ||
 		 */
-		if( ((tmpid=ipfix_name_lookup(token)) == -1) || ((id=ipfix_id_lookup(tmpid)) == NULL) ) {
+		if(isalpha((int)*token)) {
+			tmpid=ipfix_name_lookup(token);
+		} else {
+                        tmpid=atoi(token);
+		}
+
+		if( (tmpid == -1) || ((id=ipfix_id_lookup(tmpid)) == NULL) ) {
 			msg(MSG_ERROR, "Ignoring unknown template field %s", token);
                         continue;
 		}
+
 		msg(MSG_INFO, "Template: adding %s -> ID %d", token, id->id);
 		t->addField((uint16_t)id->id, (uint16_t)id->length);
 	}
 
 	free(l);
-	msg(MSG_INFO, "Got %d fields", t->getFieldCount());
+	msg(MSG_INFO, "Template: %d fields", t->getFieldCount());
 
 	return t;
 }
@@ -213,3 +281,6 @@ static void sig_handler(int x)
 	msg(MSG_DIALOG, "got signal %d - exiting", x);
 	exit(2);
 }
+
+
+static void __cplusplus_really_sucks_andPeopleUsingVeryStrangeNamingConventionsWithLongLongExplicitBlaBla(){};
