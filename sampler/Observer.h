781937742cbc1197fc22f1a1ff801d70d5a238db
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-16 17:58:33 +0000

    -Reformat Observer.{cpp,h} so one can actually read it
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@109 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/Observer.h
+++ sampler/Observer.h
@@ -1,12 +1,10 @@
 /*
- * PSAMP Reference Implementation
- *
- * Observer.h
- *
- * Declarations for observing process
- *
- * Author: Michael Drueing <michael@drueing.de>
- *
+ PSAMP Reference Implementation
+ Observer.cpp
+ Declarations for observing process
+ Author: Michael Drueing <michael@drueing.de>
+
+ changed by: Ronny T. Lampert
  */
 
 #ifndef OBSERVER_H
@@ -27,85 +25,82 @@
 #include "PacketReceiver.h"
 #include <pcap.h>
 
-class Observer
-{
+class Observer {
 public:
-  // ObserverThread constructor
-  Observer(char *interface) : thread(Observer::observerThread), exitFlag(false), capturelen(CAPTURE_LENGTH)
-  {    
-    // query all available capture devices
-    LOG("Observer: Finding devices\n");
-    pcap_findalldevs(&allDevices, errorBuffer);
-    if (!allDevices)
-    {
-      LOG("Observer: error getting list of interfaces. Reason: %s\n", errorBuffer);
-    }
-    for (pcap_if_t *dev = allDevices; dev != NULL; dev=dev->next )
-    {
-      LOG("  Name=%s, DESC=%s\n", dev->name, dev->description);
-    }
+        // ObserverThread constructor
+        Observer(char *interface) : thread(Observer::observerThread), exitFlag(false), capturelen(CAPTURE_LENGTH)
+        {
+                // query all available capture devices
+                LOG("Observer: Finding devices\n");
+                pcap_findalldevs(&allDevices, errorBuffer);
+                if(!allDevices) {
+                        LOG("Observer: error getting list of interfaces. Reason: %s\n", errorBuffer);
+                }
+                for(pcap_if_t *dev = allDevices; dev != NULL; dev=dev->next) {
+                        LOG("  Name=%s, DESC=%s\n", dev->name, dev->description);
+                }
 
-    LOG("Observer: Setting default pcap snaplen to %d\n", capturelen);
-    captureInterface=interface;
+                LOG("Observer: Setting default pcap snaplen to %d\n", capturelen);
+                captureInterface=interface;
 
-  };
+        };
 
-  ~Observer()
-  {
-    LOG("Observer: freeing devices...\n");
-    
-    if (captureDevice)
-      pcap_close(captureDevice);
-    
-    pcap_freealldevs(allDevices);
-  };
+        ~Observer()
+        {
+                LOG("Observer: freeing devices\n");
 
-  void startCapture()
-  {
-    thread.run(this);
-  };
+                if (captureDevice) {
+                        pcap_close(captureDevice);
+                }
 
-  void terminateCapture()
-  {
-    exitFlag = true;
-  };
+                pcap_freealldevs(allDevices);
+        };
 
-  void addReceiver(PacketReceiver *recv)
-  {
-    receivers.push_back(recv->getQueue());
-  };
-  
-  bool Observer::setCaptureLen(int x)
-  {
-      if(x > CAPTURE_PHYSICAL_MAX) {
-          LOG("Capture length %d exceeds physical MTU %d (with header)\n", x, CAPTURE_PHYSICAL_MAX);
-          return false;
-      }
-      capturelen=x;
+        void startCapture()
+        {
+                thread.run(this);
+        };
 
-      return true;
-  }
+        void terminateCapture()
+        {
+                exitFlag = true;
+        };
 
-  int Observer::getCaptureLen()
-  {
-      return capturelen;
-  }
+        void addReceiver(PacketReceiver *recv)
+        {
+                receivers.push_back(recv->getQueue());
+        };
+
+        bool Observer::setCaptureLen(int x)
+        {
+                if(x > CAPTURE_PHYSICAL_MAX) {
+                        LOG("Capture length %d exceeds physical MTU %d (with header)\n", x, CAPTURE_PHYSICAL_MAX);
+                        return false;
+                }
+                capturelen=x;
+                return true;
+        }
+
+        int Observer::getCaptureLen()
+        {
+                return capturelen;
+        }
 
 protected:
-  Thread thread;
-  
-  // pointer to list of pcap-devices
-  pcap_if_t *allDevices;
-  pcap_t *captureDevice;
-  char errorBuffer[PCAP_ERRBUF_SIZE];
-  int capturelen;
+        Thread thread;
 
-  static void *observerThread(void *);
+        // pointer to list of pcap-devices
+        pcap_if_t *allDevices;
+        pcap_t *captureDevice;
+        char errorBuffer[PCAP_ERRBUF_SIZE];
+        int capturelen;
+
+        static void *observerThread(void *);
 
 public:
-  bool exitFlag;
-  char *captureInterface;
-  std::vector<ConcurrentQueue<Packet> *> receivers;
+        bool exitFlag;
+        char *captureInterface;
+        std::vector<ConcurrentQueue<Packet> *> receivers;
 };
 
 #endif
