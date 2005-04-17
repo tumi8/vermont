2f75f0bfb3c0d35d49a17a7db10a693b06b4b65e
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-17 16:28:25 +0000

    -Add some fancy printing of active systems to subsys_dump()
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/trunk/vermont@117 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- subsystems.c
+++ subsystems.c
@@ -14,15 +14,34 @@ extern "C" {
 #endif
 
 /*
+ this TAB has to corespond to the SUBSYS_ defines
+ where name[i] == log2(SUBSYS_DEFINE)
+ */
+static char *TAB[]={
+	"config",
+	"conc-receiv", "conc-account", "conc-export",
+	"samp-capture", "samp-filter", "samp-sink",
+	0 };
+/*
  should print all running subsystems
- by now only dump flags
+ this could be a lot more elaborate, e.g. dumping parameters
+ and so forth, if you like
  */
 void subsys_dump(unsigned int flags)
 {
-        int size=(8*sizeof(flags));
+        int sys=0;
+	int size=(8*sizeof(flags));
 
 	msg(MSG_DIALOG, "subsystems running: 0x%x", flags);
 
+	/* do some fancy dumping */
+	while(size--) {
+		if(flags & 0x1) {
+                        msg(MSG_DIALOG, "[X] %s", TAB[sys]);
+		}
+		flags >>= 1;
+		sys++;
+	}
 }
 
 
