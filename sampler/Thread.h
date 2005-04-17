9dc1aace3d1c20d8c91a2ac1394a73c95bc8fa24
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-17 17:06:12 +0000

    -Make Thread.h somewhat more readable
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@118 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/Thread.h
+++ sampler/Thread.h
@@ -20,40 +20,40 @@ typedef void* (*ThreadFunc)(void *);
 class Thread
 {
 public:
-  Thread(ThreadFunc threadFunction) : exitFlag(false), f(threadFunction)
-  {
-  };
+	Thread(ThreadFunc threadFunction) : exitFlag(false), f(threadFunction)
+	{
+	};
 
-  bool run(void *threadData)
-  {
-    //data = threadData;
-    return pthread_create(&thread, NULL, f, threadData) == 0;
-  };
+bool run(void *threadData)
+{
+	//data = threadData;
+	return pthread_create(&thread, NULL, f, threadData) == 0;
+};
 
-  void *join()
-  {
-    void *result = NULL;
-    if (pthread_join(thread, &result))
-      LOG("Thread: ERROR: Join failed\n");
-      
-    return result;
-  };
+void *join()
+{
+	void *result = NULL;
+	if (pthread_join(thread, &result))
+		LOG("Thread: ERROR: Join failed\n");
 
-  bool detach()
-  {
-    return (pthread_detach(thread) == 0);
-  }
-  
-  ~Thread()
-  {
-  };
+	return result;
+};
 
-  bool exitFlag;
-  //void *data;
-  
+bool detach()
+{
+	return (pthread_detach(thread) == 0);
+}
+
+~Thread()
+{
+};
+
+bool exitFlag;
+//void *data;
+
 private:
-  pthread_t thread;
-  ThreadFunc f;
+	pthread_t thread;
+	ThreadFunc f;
 };
 
 #endif
