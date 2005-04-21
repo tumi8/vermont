540508c214962fa33513af4dce4c2d2e30e8edb9
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-21 11:49:12 +0000

    -Clean up Filter o make startFilter() bool to return errors from thread.run() o use msg() subsystem or DPRINTF() o make is actually readable -Add some more msg()s to show whats going on
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@137 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/Filter.cpp
+++ sampler/Filter.cpp
@@ -8,7 +8,7 @@
  * Author: Michael Drueing <michael@drueing.de>
  *
  */
- 
+#include "msg.h"
 #include "Filter.h"
 
 using namespace std;
@@ -17,37 +17,33 @@ Filter *Filter::instance = 0;
 
 void *Filter::filterProcess(void *arg)
 {
-  Filter *filter = Filter::instance;
-  Packet *p;
-  bool keepPacket;
-  vector<PacketProcessor *>::iterator it;
+        Filter *filter = Filter::instance;
+        Packet *p;
+        bool keepPacket;
+        vector<PacketProcessor *>::iterator it;
 
-  while (!filter->exitFlag)
-  {
-    // get a packet...
-    p = filter->getQueue()->pop();
-    filter->pktIn++;
-    
-    // run packet through all packetProcessors
-    for (it = filter->processors.begin(); it != filter->processors.end(); ++it)
-    {
-      keepPacket = (*it)->processPacket(p);
-      if (!keepPacket)
-        break;
-    }
-    
-    //check if we passed all filters
-    if (keepPacket)
-    {
-      // get the packet to the receiver
-      filter->receiver->push(p);
-      filter->pktOut++;
-    }
-    else
-    {
-      // immediately drop the packet
-      p->release();
-    }
-  }
+        msg(MSG_INFO, "Filter: now running the filter thread");
+        while(!filter->exitFlag) {
+                // get a packet...
+                p = filter->getQueue()->pop();
+                filter->pktIn++;
+
+                // run packet through all packetProcessors
+                for(it = filter->processors.begin(); it != filter->processors.end(); ++it) {
+                        keepPacket = (*it)->processPacket(p);
+                        if (!keepPacket)
+                                break;
+                }
+
+                //check if we passed all filters
+                if(keepPacket) {
+                        // get the packet to the receiver
+                        filter->receiver->push(p);
+                        filter->pktOut++;
+                } else {
+                        // immediately drop the packet
+                        p->release();
+                }
+        }
 };
 
