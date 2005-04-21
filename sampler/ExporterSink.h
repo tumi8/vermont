5708f5d52658b4496bd3a044945d8c5efb905c95
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-21 12:51:17 +0000

    -Cleanup ExporterSink o use msg() o return thread.run returncodes -Formatting fixes
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@139 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/ExporterSink.h
+++ sampler/ExporterSink.h
@@ -9,14 +9,16 @@
  * Author: Michael Drueing <michael@drueing.de>
  *
  */
- 
+
 #ifndef EXPORTER_SINK_H
 #define EXPORTER_SINK_H
+
+#include "ipfixlolib/ipfixlolib.h"
+#include "msg.h"
 
 #include "PacketReceiver.h"
 #include "Thread.h"
 #include "Template.h"
-#include "ipfixlolib/ipfixlolib.h"
 
 // the maximum number of packets to be queued
 #define MAX_PACKETS 1024
@@ -26,140 +28,144 @@
 class ExporterSink : public PacketReceiver
 {
 public:
-    ExporterSink(Template *tmpl, int sID) : sourceID(sID),
-        templ(tmpl), thread(ExporterSink::exporterSinkProcess), exitFlag(false),
-        numPacketsToRelease(0), ipfix_maxpackets(IPFIX_PACKETS_MAX)
-  {
-    int ret, i, tmplid;
-    unsigned short ttype, tlength, toffset;
-    
-    // generate the exporter
-    ret = ipfix_init_exporter(sourceID, &exporter);
-    if (ret)
-    {
-      LOG("Error initializing IPFIX exporter\n");
-      exit(1);
-    }
-    
-    // generate the ipfix template
-    tmplid = templ->getTemplateID();
-    ret =  ipfix_start_template_set(exporter, tmplid, templ->getFieldCount());
-    for (i = 0; i < templ->getFieldCount(); i++)
-    {
-      templ->getFieldInfo(i, &ttype, &tlength, &toffset);
-      ipfix_put_template_field(exporter, tmplid, ttype, tlength, 0);
-    }
-    ipfix_end_template_set(exporter, tmplid);
-  };
-  
-  void AddCollector(char *address, unsigned short port, const char *protocol)
-  {
-    ipfix_transport_protocol proto;
-    
-    if (strcasecmp(protocol, "TCP") == 0)
-      proto = TCP;
-    else if (strcasecmp(protocol, "UDP") == 0)
-      proto = UDP;
-    else
-      LOG("Invalid protocol %s\n", protocol);
-      
-    LOG("Adding %s://%s:%d\n", protocol, address, port);
-    ipfix_add_collector(exporter, address, port, proto);
-  }
+        ExporterSink(Template *tmpl, int sID) : sourceID(sID),
+                templ(tmpl), thread(ExporterSink::exporterSinkProcess), exitFlag(false),
+                numPacketsToRelease(0), ipfix_maxpackets(IPFIX_PACKETS_MAX)
+        {
+                int ret, i, tmplid;
+                unsigned short ttype, tlength, toffset;
 
-  ~ExporterSink()
-  {
-    ipfix_deinit_exporter(exporter);
-  };
+                // generate the exporter
+                ret = ipfix_init_exporter(sourceID, &exporter);
+                if(ret) {
+                        msg(MSG_FATAL, "ExporterSink: error initializing IPFIX exporter");
+                        exit(1);
+                }
 
-  inline void runSink()
-  {
-    thread.run(this);
-  };
+                // generate the ipfix template
+                tmplid = templ->getTemplateID();
+                ret =  ipfix_start_template_set(exporter, tmplid, templ->getFieldCount());
 
-  inline void terminateSink()
-  {
-    exitFlag = true;
-  };
-  
-  // the usage of the following functions is:
-  //
-  // StartPacketStream();
-  // while (more_packets AND timeout_not_met)
-  // {
-  //   AddPaket(getNextPacket());
-  // }
-  // FlushPacketStream();
-  
-  // start a new IPFIX packet stream
-  void StartNewPacketStream()
-  {
-    unsigned short net_tmplid = htons(templ->getTemplateID());
-    LOG("Starting new packet stream\n");
-    numPacketsToRelease = 0;
-    ipfix_start_data_set(exporter, &net_tmplid);
-  }
-  
-  // Add this packet to the packet stream
-  void AddPacket(Packet *pck)
-  {
-    unsigned short ttype, tlength, toffset;
-    // first, store the packet to be released later, after we have sent the data
-    LOG("Adding packet to stream\n");
-    packetsToRelease[numPacketsToRelease++] = pck;
-    
-    for (int i = 0; i < templ->getFieldCount(); i++)
-    {
-      templ->getFieldInfo(i, &ttype, &tlength, &toffset);
-      ipfix_put_data_field(exporter, pck->getPacketData(toffset), tlength);
-    }
-  }
-  
-  // send out the IPFIX packet stream and reset
-  void FlushPacketStream()
-  {
-    // end the packet stream and send the IPFIX packet out through the wire
-    ipfix_end_data_set(exporter);
-    ipfix_send(exporter);
-    
-    LOG("Flushing %d packets from stream\n", numPacketsToRelease);
-    for (int i = 0; i < numPacketsToRelease; i++)
-    {
-      (packetsToRelease[i])->release();
-    }
-    numPacketsToRelease = 0;
-  }
+                for(i = 0; i < templ->getFieldCount(); i++) {
+                        templ->getFieldInfo(i, &ttype, &tlength, &toffset);
+                        ipfix_put_template_field(exporter, tmplid, ttype, tlength, 0);
+                }
+
+                ipfix_end_template_set(exporter, tmplid);
+        };
+
+        bool AddCollector(char *address, unsigned short port, const char *protocol)
+        {
+                ipfix_transport_protocol proto;
+
+                if (strcasecmp(protocol, "TCP") == 0) {
+                        proto = TCP;
+                } else if(strcasecmp(protocol, "UDP") == 0) {
+                        proto = UDP;
+                } else {
+                        msg(MSG_ERROR, "ExporterSink: invalid protocol %s for %s",
+                            protocol, address
+                           );
+                        return false;
+                }
+
+                DPRINTF("Adding %s://%s:%d\n", protocol, address, port);
+                return(ipfix_add_collector(exporter, address, port, proto) == 0);
+        }
+
+        ~ExporterSink()
+        {
+                ipfix_deinit_exporter(exporter);
+        };
+
+        bool runSink()
+        {
+                msg(MSG_DEBUG, "Sink: now starting ExporterSink thread");
+                return (thread.run(this));
+        };
+
+        void terminateSink()
+        {
+                exitFlag = true;
+        };
+
+        // the usage of the following functions is:
+        //
+        // StartPacketStream();
+        // while (more_packets AND timeout_not_met)
+        // {
+        //   AddPaket(getNextPacket());
+        // }
+        // FlushPacketStream();
+
+        // start a new IPFIX packet stream
+        void StartNewPacketStream()
+        {
+                unsigned short net_tmplid = htons(templ->getTemplateID());
+                DPRINTF("Starting new packet stream\n");
+                numPacketsToRelease = 0;
+                ipfix_start_data_set(exporter, &net_tmplid);
+        }
+
+        // Add this packet to the packet stream
+        void AddPacket(Packet *pck)
+        {
+                unsigned short ttype, tlength, toffset;
+                // first, store the packet to be released later, after we have sent the data
+                DPRINTF("Adding packet to stream\n");
+                packetsToRelease[numPacketsToRelease++] = pck;
+
+                for(int i = 0; i < templ->getFieldCount(); i++) {
+                        templ->getFieldInfo(i, &ttype, &tlength, &toffset);
+                        ipfix_put_data_field(exporter, pck->getPacketData(toffset), tlength);
+                }
+        }
+
+        // send out the IPFIX packet stream and reset
+        void FlushPacketStream()
+        {
+                // end the packet stream and send the IPFIX packet out through the wire
+                ipfix_end_data_set(exporter);
+                ipfix_send(exporter);
+
+                DPRINTF("Flushing %d packets from stream\n", numPacketsToRelease);
+                for (int i = 0; i < numPacketsToRelease; i++)
+                {
+                        (packetsToRelease[i])->release();
+                }
+                numPacketsToRelease = 0;
+        }
 
 
-  /* max packets per big IPFIX packet */
-  bool setMaxIpfixPP(int x)
-  {
-      ipfix_maxpackets=x;
+        /* max packets per big IPFIX packet */
+        bool setMaxIpfixPP(int x)
+        {
+                ipfix_maxpackets=x;
 
-      return true;
-  }
+                return true;
+        }
 
-  int getMaxIpfixPP()
-  {
-      return ipfix_maxpackets;
-  }
+        int getMaxIpfixPP()
+        {
+                return ipfix_maxpackets;
+        }
 
 protected:
-  int sourceID;
-  Template *templ;
-  Thread thread;
-  static void *exporterSinkProcess(void *);
-  
-  ipfix_exporter *exporter;
+        int sourceID;
+        Template *templ;
+        Thread thread;
+        static void *exporterSinkProcess(void *);
 
-  // these packets need to be release()'d after we send the current IPFIX packet
-  int numPacketsToRelease;
-  Packet *packetsToRelease[MAX_PACKETS];
+        ipfix_exporter *exporter;
 
-  int ipfix_maxpackets;
+        // these packets need to be release()'d after we send the current IPFIX packet
+        int numPacketsToRelease;
+        Packet *packetsToRelease[MAX_PACKETS];
+
+        int ipfix_maxpackets;
 
 public:
-  bool exitFlag;
+        bool exitFlag;
 };
 
 #endif
