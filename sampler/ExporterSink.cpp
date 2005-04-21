1d99cfdb7caa31fa572613fd7881acaab8d1a30c
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-21 12:51:17 +0000

    -Cleanup ExporterSink o use msg() o return thread.run returncodes -Formatting fixes
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/trunk/vermont@139 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/ExporterSink.cpp
+++ sampler/ExporterSink.cpp
@@ -9,43 +9,48 @@
  * Author: Michael Drueing <michael@drueing.de>
  *
  */
- 
+
+/*
+ TODO
+ FIXME: NO TIME CONSTRAINTS HERE
+ TODO
+ */
+
+#include "msg.h"
+#include "ipfixlolib/ipfixlolib.h"
+
 #include "ExporterSink.h"
 #include "Globals.h"
-
-extern "C" {
-#include "ipfixlolib/ipfixlolib.h"
-}
 
 using namespace std;
 
 void *ExporterSink::exporterSinkProcess(void *arg)
 {
-  ExporterSink *sink = (ExporterSink *)arg;
-  ConcurrentQueue<Packet> *queue = sink->getQueue();
-  Packet *p;
-  int pckCount = 0;
-  int deadline = 400; // timeout in msec after first packet has been added
-  
-  LOG("ExporterSink started\n");
-  while (!sink->exitFlag)
-  {
-    pckCount = 1;
-    
-    // first we need to get a packet
-    p = queue->pop();
-    sink->StartNewPacketStream();
-    sink->AddPacket(p);
-    
-    while (pckCount < sink->ipfix_maxpackets)
-    {
-      // TODO: add time constraint here (max. wait time)
-      p = queue->pop(); 
-      // if (timeout) break;
-      sink->AddPacket(p);
-      pckCount++;
-    }
-    // TODO: add packets here with time constraints
-    sink->FlushPacketStream();
-  }
+        ExporterSink *sink = (ExporterSink *)arg;
+        ConcurrentQueue<Packet> *queue = sink->getQueue();
+        Packet *p;
+
+        int pckCount = 0;
+        int deadline = 400; // timeout in msec after first packet has been added
+
+        msg(MSG_INFO, "Sink: running ExporterSink");
+
+        while(!sink->exitFlag) {
+                pckCount = 1;
+
+                // first we need to get a packet
+                p = queue->pop();
+                sink->StartNewPacketStream();
+                sink->AddPacket(p);
+
+                while(pckCount < sink->ipfix_maxpackets) {
+                        // TODO: add time constraint here (max. wait time)
+                        p = queue->pop();
+                        // if (timeout) break;
+                        sink->AddPacket(p);
+                        pckCount++;
+                }
+                // TODO: add packets here with time constraints
+                sink->FlushPacketStream();
+        }
 }
