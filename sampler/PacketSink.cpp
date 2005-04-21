4b0b344027f73f15f3f80d55ee5b748b9d600236
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-21 12:36:03 +0000

    -Cleanup PacketSink o Use msg() subsystem -Make runSink() bool and return thread.run() returncode
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@138 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/PacketSink.cpp
+++ sampler/PacketSink.cpp
@@ -8,24 +8,24 @@
  * Author: Michael Drueing <michael@drueing.de>
  *
  */
-
 #include <iostream>
+
+#include "msg.h"
 #include "PacketSink.h"
 
 using namespace std;
 
 void *PacketSink::packetSinkProcess(void *arg)
 {
-  PacketSink *sink = (PacketSink *)arg;
-  ConcurrentQueue<Packet> *queue = sink->getQueue();
-  Packet *p;
-  
-  cerr << "PacketSink started" << endl;
-  while (!sink->exitFlag)
-  {
-    p = queue->pop();
-    p->release();
-    //cerr << "*";
-  }
+        PacketSink *sink = (PacketSink *)arg;
+        ConcurrentQueue<Packet> *queue = sink->getQueue();
+        Packet *p;
+
+        msg(MSG_INFO, "Sink: now running PacketSink thread");
+        while(!sink->exitFlag) {
+                p = queue->pop();
+                p->release();
+                //DPRINTF("SINK: free packet");
+        }
 }
 
