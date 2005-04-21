2754d87701d64b681bf6c23cbdb2c1be9e0919de
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-04-21 11:49:12 +0000

    -Clean up Filter o make startFilter() bool to return errors from thread.run() o use msg() subsystem or DPRINTF() o make is actually readable -Add some more msg()s to show whats going on
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/trunk/vermont@137 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/Filter.h
+++ sampler/Filter.h
@@ -8,11 +8,13 @@
  * Author: Michael Drueing <michael@drueing.de>
  *
  */
- 
+
 #ifndef FILTER_H
 #define FILTER_H
 
 #include <vector>
+#include "msg.h"
+
 #include "Globals.h"
 #include "ConcurrentQueue.h"
 #include "Packet.h"
@@ -26,58 +28,64 @@
 class Filter : public PacketReceiver
 {
 public:
-  Filter() : thread(Filter::filterProcess), exitFlag(false),pktIn(0), pktOut(0)
-  {
-  }
-   
-  ~Filter()
-  {
-  }
-  
-  void startFilter()
-  {
-    Filter::instance = this;
-    thread.run(0);
-  }
-  
-  void terminate()
-  {
-    exitFlag = true;
-  }
-  
-  // adds a new output queue to the receivers
-  void setReceiver(PacketReceiver *recv)
-  {
-    receiver = recv->getQueue();
-  };
-  
-  // add a new filter or sampler
-  void addProcessor(PacketProcessor *p)
-  {
-    processors.push_back(p);
-  };
+        Filter() : thread(Filter::filterProcess), exitFlag(false),pktIn(0), pktOut(0)
+        {
+        }
 
-  /* in case we have to do some things with 'em */
-  std::vector<PacketProcessor *> getProcessors()
-  {
-          return processors;
-  }
+        ~Filter()
+        {
+        }
+
+        bool startFilter()
+        {
+                Filter::instance = this;
+
+                msg(MSG_DEBUG, "Filter: now starting Filter thread");
+                return(thread.run(0));
+        }
+
+        void terminate()
+        {
+                exitFlag = true;
+        }
+
+        // adds a new output queue to the receivers
+        void setReceiver(PacketReceiver *recv)
+        {
+                receiver = recv->getQueue();
+        };
+
+        // add a new filter or sampler
+        void addProcessor(PacketProcessor *p)
+        {
+                processors.push_back(p);
+        };
+
+        /* in case we have to do some things with 'em */
+        std::vector<PacketProcessor *> getProcessors()
+        {
+                return processors;
+        }
 
 protected:
-  Thread thread;
-  static void *filterProcess(void *);
-  std::vector<PacketProcessor *> processors;
-  //std::vector<ConcurrentQueue<Packet> *> receivers;
-  ConcurrentQueue<Packet> *receiver;
-  
+        Thread thread;
+        static void *filterProcess(void *);
+        std::vector<PacketProcessor *> processors;
+        //std::vector<ConcurrentQueue<Packet> *> receivers;
+        ConcurrentQueue<Packet> *receiver;
+
 public:
-  bool exitFlag;
-  
-  //debug variables
-  unsigned long pktIn;
-  unsigned long pktOut;
-  
-  static Filter *Filter::instance;
+        bool exitFlag;
+
+        //debug variables
+        unsigned long pktIn;
+        unsigned long pktOut;
+
+        /*
+         FIXME: THIS DESIGN SUCKS!
+         Only one Filtering process per object
+         */
+        static Filter *Filter::instance;
 };
 
 #endif
