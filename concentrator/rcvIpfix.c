a7579487ef8f45b19e84fee322623defa13549e3
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-05-16 11:42:23 +0000

    -Compile fix for rcvIpfix.c o ntohll et al are in ipfixlolib, so include
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@210 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- concentrator/rcvIpfix.c
+++ concentrator/rcvIpfix.c
@@ -47,6 +47,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include <netdb.h>
 #include <unistd.h>
 
+/* for ntohll et al */
+#include "ipfixlolib/ipfixlolib.h"
+
 #include "rcvIpfix.h"
 #include "templateBuffer.h"
 #include "ipfix.h"
