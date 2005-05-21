74ee87540015f0cc88887ee3fd911b57edb3e799
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-05-21 13:08:31 +0000

    -Misc. + formatting fixes for SystematicSampler
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@249 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/SystematicSampler.cpp
+++ sampler/SystematicSampler.cpp
@@ -13,26 +13,28 @@
 
 bool SystematicSampler::processPacket(const Packet *p)
 {
-  struct timeval elapsed;
-  unsigned long msecs;
-  
-  // calculate time elapsed since start of sampling
-  timersub(&p->timestamp, &this->startTime, &elapsed);
-  
-  // calculate number of milliseconds
-  msecs = (elapsed.tv_sec * 1000) + (elapsed.tv_usec / 1000);
-    
-  packetCount++;
-  
-  switch(samplingType) {
-  case SYSTEMATIC_SAMPLER_TIME_BASED:
-	// normalize to interval borders and check if it's <= onTime
-	return ((msecs % interval) < samplingOnTime);
-	break;
-  case SYSTEMATIC_SAMPLER_COUNT_BASED:
- 	return ((packetCount % interval) < samplingOnTime);
-	break;
-  }
+	struct timeval elapsed;
+	unsigned long msecs;
+
+	// calculate time elapsed since start of sampling
+	timersub(&p->timestamp, &this->startTime, &elapsed);
+
+	// calculate number of milliseconds
+	msecs = (elapsed.tv_sec * 1000) + (elapsed.tv_usec / 1000);
+
+	packetCount++;
+
+	switch(samplingType) {
+
+	case SYSTEMATIC_SAMPLER_TIME_BASED:
+		// normalize to interval borders and check if it's <= onTime
+		return ((msecs % interval) < samplingOnTime);
+		break;
+
+	case SYSTEMATIC_SAMPLER_COUNT_BASED:
+		return ((packetCount % interval) < samplingOnTime);
+		break;
+	}
 
 
 
--- sampler/SystematicSampler.h
+++ sampler/SystematicSampler.h
@@ -8,7 +8,7 @@
  * Author: Michael Drueing <michael@drueing.de>
  *
  */
- 
+
 #ifndef SYSTEMATIC_SAMPLER_H
 #define SYSTEMATIC_SAMPLER_H
 
@@ -22,34 +22,37 @@ class SystematicSampler : public PacketProcessor
 {
 public:
 
-  // constructs a new systematic sampler (see section 3.1.1 of PSAMP-Sample-Tech)
-  // if type is CountBasedSampler then onTime and offTime specify how long to sample.
-  // for example if onTime = 10 and offTime = 90, we sample 10 packets out of 100
-  // if type is TimeBasedSampler, then onTime and offTime are specified in milliseconds
-  // and specify how long to keep capturing packets.
-  SystematicSampler(int type, unsigned long onTime, unsigned long offTime) :
-      samplingType(type), samplingOnTime(onTime), samplingOffTime(offTime), packetCount(0)
-  {
-    gettimeofday(&startTime, 0);
-    
-    interval = samplingOnTime + samplingOffTime;
-  };
+        /*
+         constructs a new systematic sampler (see section 3.1.1 of PSAMP-Sample-Tech)
+         if type is CountBasedSampler then onTime and offTime specify how long to sample.
+         for example if onTime = 10 and offTime = 90, we sample 10 packets out of 100
+         if type is TimeBasedSampler, then onTime and offTime are specified in milliseconds
+         and specify how long to keep capturing packets.
+         */
+        SystematicSampler(int type, unsigned long onTime, unsigned long offTime) :
+                samplingType(type), samplingOnTime(onTime), samplingOffTime(offTime), packetCount(0)
+        {
+                gettimeofday(&startTime, 0);
 
-  ~SystematicSampler();
+                interval = samplingOnTime + samplingOffTime;
+        };
 
-  virtual bool processPacket(const Packet *p);
-  
+        ~SystematicSampler();
+
+        virtual bool processPacket(const Packet *p);
+
 protected:
-  int samplingType;
-  unsigned long samplingOnTime;
-  unsigned long samplingOffTime;
-  unsigned long interval;
-  
-  // for time-based sampler:
-  struct timeval startTime;
-  
-  // for count-based samplers
-  unsigned long packetCount;
+        int samplingType;
+
+        unsigned long samplingOnTime;
+        unsigned long samplingOffTime;
+        unsigned long interval;
+
+        // for time-based sampler:
+        struct timeval startTime;
+
+        // for count-based samplers
+        unsigned long packetCount;
 };
 
 #endif
