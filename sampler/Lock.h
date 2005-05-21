408bcfd36b1b50bdcc8e48cf370f5b543bab5b2f
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-05-21 13:43:52 +0000

    -Misc. formatting fixes for Lock
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@255 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/Lock.h
+++ sampler/Lock.h
@@ -17,38 +17,40 @@
 class Lock
 {
 private:
-  // the system mutex used (currently using pthread mutexes)
-  pthread_mutex_t mutex;
-  
+	// the system mutex used (currently using pthread mutexes)
+	pthread_mutex_t mutex;
+
 public:
-  // constructs a new Lock
-  inline Lock()
-  {
-    pthread_mutex_init(&mutex, NULL);
-  };
-  
-  // deletes a Lock.
-  // TODO: use try_lock to see if lock is currently held. If so,
-  // then results are unpredictable!
-  inline ~Lock()
-  {
-    pthread_mutex_destroy(&mutex);
-  };
-  
-  inline void lock()
-  {
-    pthread_mutex_lock(&mutex);
-  };
+	// constructs a new Lock
+	inline Lock()
+	{
+		pthread_mutex_init(&mutex, NULL);
+	};
 
-  inline void unlock()
-  {
-    pthread_mutex_unlock(&mutex);
-  };
+        /*
+         deletes a Lock.
+         TODO: use try_lock to see if lock is currently held.
+         If so, then results are unpredictable!
+         */
+	inline ~Lock()
+	{
+		pthread_mutex_destroy(&mutex);
+	};
 
-  inline bool try_lock()
-  {
-    return (pthread_mutex_trylock(&mutex) == 0);
-  };
+	inline void lock()
+	{
+		pthread_mutex_lock(&mutex);
+	};
+
+	inline void unlock()
+	{
+		pthread_mutex_unlock(&mutex);
+	};
+
+	inline bool try_lock()
+	{
+		return (pthread_mutex_trylock(&mutex) == 0);
+	};
 };
 
 #endif
