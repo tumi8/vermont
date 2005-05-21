20f22f4ebeb1aa4c101583ebdacd4319fb78931c
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-05-21 13:52:12 +0000

    -BUGFIX: Filter was comparing uninitialized values: srcvalue was never initialized! -Added comments
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@256 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/IPHeaderFilter.h
+++ sampler/IPHeaderFilter.h
@@ -30,7 +30,7 @@
 // less than
 #define CMP_LT  0x00
 // less or equal
-#define CMP_LE  0x01  
+#define CMP_LE  0x01
 // equal
 #define CMP_EQ  0x02
 // greater or equal
@@ -45,31 +45,34 @@
 class IPHeaderFilter : public PacketProcessor
 {
 public:
-        IPHeaderFilter(int header, int offset, int size, int comparison, int value)
-                : m_offset(offset), m_size(size), m_comparison(comparison), m_value(value)
-        {
-        }
+	IPHeaderFilter(int header, int offset, int size, int comparison, int value)
+		: m_offset(offset), m_size(size), m_comparison(comparison), m_value(value)
+	{
+		if(!(size==1 || size==2 || size==4)) {
+			msg(MSG_ERROR, "IPHeaderFilter: invalid size of %d, only 1/2/4 supported", size);
+		}
+	}
 
-        virtual bool processPacket(const Packet *p);
+	virtual bool processPacket(const Packet *p);
 
 protected:
-        bool compareValues(int srcvalue, int dstvalue);
-        int getData(void *data, int size);
+	bool compareValues(int srcvalue, int dstvalue);
+	int getData(void *data, int size);
 
-        /* which header: IP or transport */
-        int m_header;
+	/* which header: IP or transport */
+	int m_header;
 
-        /* offset from the header above */
-        int m_offset;
+	/* offset from the header above */
+	int m_offset;
 
-        /* the size one wants to compare - 1, 2 or 4 bytes */
-        int m_size;
+	/* the size one wants to compare - 1, 2 or 4 bytes */
+	int m_size;
 
-        /* the comparison operation, see the CMP_ defines above */
-        int m_comparison;
+	/* the comparison operation, see the CMP_ defines above */
+	int m_comparison;
 
-        /* the value to compare against */
-        int m_value;
+	/* the value to compare against */
+	int m_value;
 };
 
 #endif
