dc70fbec354bd5b823cbdbb49febbbf006e62a24
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-05-21 12:54:04 +0000

    -Misc. formatting etc. cleanups to RandomSampler
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/trunk/vermont@246 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/RandomSampler.cpp
+++ sampler/RandomSampler.cpp
@@ -8,7 +8,7 @@
  * Author: Michael Drueing <michael@drueing.de>
  *
  */
- 
+
 #include <cstdlib>
 #include <ctime>
 
@@ -16,38 +16,40 @@
 
 RandomSampler::RandomSampler(int n, int N) : samplingSize(N), acceptSize(n), currentPos(0)
 {
-  int pos;
-  
-  if (n > N)
-  {
-    fprintf(stderr, "RandomSampler: %d-out-of%d makes no sense!\n", n, N);
-    exit(3);
-  }
-  
-  sampleMask.clear();
-  sampleMask.insert(sampleMask.begin(), N, false);
-  
-  srand(time(0));
-  
-  // setup sampling bitfield
-  // TODO: There might be a more elegant solution to this...
-  for (int i = 0; i < acceptSize; i++)
-  {
-    // find a free spot in the sampleMask (i.e. a position
-    // with FALSE in it) and set it to TRUE
-    do
-    {
-      pos = rand() % samplingSize;
-    } while (sampleMask[pos]);
-    sampleMask[pos] = true;
-  }
+        int pos;
+
+        if (n > N) {
+                msg(MSG_ERROR, "RandomSampler: %d out-of %d makes no sense", n, N);
+                return NULL;
+        }
+
+        sampleMask.clear();
+        sampleMask.insert(sampleMask.begin(), N, false);
+
+        srand(time(0));
+
+        /*
+         setup sampling bitfield
+         TODO: There might be a more elegant solution to this...
+         */
+        for(int i = 0; i < acceptSize; i++) {
+                /*
+                 find a free spot in the sampleMask (i.e. a position
+                 with FALSE in it) and set it to TRUE
+                 */
+                do {
+                        pos = rand() % samplingSize;
+                } while(sampleMask[pos]);
+
+                sampleMask[pos] = true;
+        }
 };
 
 bool RandomSampler::processPacket(const Packet *p)
 {
-  bool accepted = sampleMask[currentPos];
-  
-  currentPos = (currentPos + 1) % samplingSize;
-  
-  return accepted;
+        bool accepted = sampleMask[currentPos];
+
+        currentPos = (currentPos + 1) % samplingSize;
+
+        return accepted;
 }
