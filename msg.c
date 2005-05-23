a103c805ba08235e8c066b81a2f42a450cdad15d
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-05-23 13:34:49 +0000

    -Add comments about msg() subsystem and custom backends and the need of locks
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/trunk/vermont@266 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- msg.c
+++ msg.c
@@ -16,6 +16,14 @@ extern "C" {
 static int msg_level=MSG_DEFAULT;
 static char *MSG_TAB[]={ "FATAL  ", "VERMONT", "ERROR  ", "DEBUG  ", "INFO   ", 0};
 
+/*
+ the main logging routine
+
+ we don't have to lock, because glibc's printf() system is doing it already
+
+ if you, however, change this function to a custom backend, you WILL have to lock
+ because msg() can and will be called by concurrent threads!
+ */
 void msg(int level, char *fmt, ...)
 {
 	/* nummerically higher value means lower priority */
