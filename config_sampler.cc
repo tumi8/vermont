44212c04bf91a9a3854de2ea563bc09068b5056a
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-06-01 14:30:05 +0000

    -Cleanup config_sampler o remove unused variable o rework configure_sink() to not use a char *list
    -Add URI parsing to config_sampler
    o multiple exports up to 16 supported
    
    
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@334 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- config_sampler.cc
+++ config_sampler.cc
@@ -25,6 +25,7 @@
 #include "vermont.h"
 #include "iniparser.h"
 #include "msg.h"
+#include "uri.h"
 #include "subsystems.h"
 #include "config_sampler.h"
 
@@ -34,7 +35,7 @@ static char *CONF_SEC="sampler";
 static int configure_observer(struct v_objects *v, char *interface, int snaplen);
 static int configure_template(struct v_objects *v, uint16_t template_id, char *list);
 static int configure_filter(struct v_objects *v, char *list);
-static int configure_sink(struct v_objects *v, char *list);
+static int configure_sink(struct v_objects *v);
 static PacketProcessor * make_filter_processor(char *name, char *setting);
 
 /*
@@ -51,71 +52,68 @@ int configure_sampler(struct v_objects *v)
 {
 	dictionary *conf=v->v_config;
 
-        msg(MSG_DEBUG, "Config: now configuring the sampler subsystem");
+	msg(MSG_DEBUG, "Config: now configuring the sampler subsystem");
 
-        /* dont configure a template if simply using PacketSink -- export_ip=off */
-        if(strcasecmp("off", iniparser_getvalue(conf, CONF_SEC, "export_ip")) == 0) {
-                msg(MSG_INFO, "Config: Template will not be build, because export_ip=off");
+	/* dont configure a template if simply using PacketSink -- export_ip=off */
+	if(strcasecmp("off", iniparser_getvalue(conf, CONF_SEC, "export_to")) == 0) {
+		msg(MSG_INFO, "Config: Template will not be build, because export_ip=off");
 
-        } else if(configure_template(
-                                     v,
-                                     atoi(iniparser_getvalue(conf, CONF_SEC, "template_id")),
-                                     iniparser_getvalue(conf, CONF_SEC, "template")
-                                    )) {
-                msg(MSG_FATAL, "Config: could not configure a template");
-                return 1;
-        }
+	} else if(configure_template(
+				     v,
+				     atoi(iniparser_getvalue(conf, CONF_SEC, "template_id")),
+				     iniparser_getvalue(conf, CONF_SEC, "template")
+				    )) {
+		msg(MSG_FATAL, "Config: could not configure a template");
+		return 1;
+	}
 
-        /*
-         configure an observer
-         will caputure packets
-         */
-        if(configure_observer(
-			       v,
-			       iniparser_getvalue(conf, CONF_SEC, "interface"),
-			       atoi(iniparser_getvalue(conf, CONF_SEC, "capturelen"))
-			      )) {
+	/*
+	 configure an observer
+	 will caputure packets
+	 */
+	if(configure_observer(
+			      v,
+			      iniparser_getvalue(conf, CONF_SEC, "interface"),
+			      atoi(iniparser_getvalue(conf, CONF_SEC, "capturelen"))
+			     )) {
 		msg(MSG_FATAL, "Config: could not configure an observer(pcap)");
 		return 1;
 	}
-        subsys_on(&(v->v_subsystems), SUBSYS_SAMP_CAPTURE);
+	subsys_on(&(v->v_subsystems), SUBSYS_SAMP_CAPTURE);
 
-        /*
-         configure a filter process
-         make main filter object
-         attach packetprocessors
-         */
+	/*
+	 configure a filter process
+	 make main filter object
+	 attach packetprocessors
+	 */
 
-        if(configure_filter(
-			     v,
-			     iniparser_getvalue(conf, CONF_SEC, "filters")
-			    )) {
+	if(configure_filter(
+			    v,
+			    iniparser_getvalue(conf, CONF_SEC, "filters")
+			   )) {
 
 		msg(MSG_FATAL, "Config: could not configure the filter");
-                return 1;
+		return 1;
 	}
-        subsys_on(&(v->v_subsystems), SUBSYS_SAMP_FILTER);
+	subsys_on(&(v->v_subsystems), SUBSYS_SAMP_FILTER);
 
-        /*
-         we MUST have a sink, else we will memleak like hell
+	/*
+	 we MUST have a sink, else we will memleak like hell
 
-         configure_sink() takes care to allocate one of:
-         ExporterSink() -> export IPFIX data
-         PacketSink() -> just free packets
-         */
-        /*
-         BIG FIXME: FIX PARAMETERS!
-         */
-        if(configure_sink(
-                          v,
-                          iniparser_getvalue(conf, CONF_SEC, "export_ip")
-                         )) {
+	 configure_sink() takes care to allocate one of:
+	 ExporterSink() -> export IPFIX data
+	 PacketSink() -> just free packets
+	 */
+	/*
+	 BIG FIXME: FIX PARAMETERS!
+	 */
+	if(configure_sink(v)) {
 		msg(MSG_FATAL, "Config: could not configure a packet sink (important)");
-                return 1;
-        }
-        subsys_on(&(v->v_subsystems), SUBSYS_SAMP_SINK);
+		return 1;
+	}
+	subsys_on(&(v->v_subsystems), SUBSYS_SAMP_SINK);
 
-        /*
+	/*
 	 now do virtual wiring:
 	 observer --> filter --> sink
 
@@ -127,10 +125,10 @@ int configure_sampler(struct v_objects *v)
 	assert(v->sink != 0);
 
 	v->observer->addReceiver(v->filter);
-        v->filter->setReceiver(v->sink);
+	v->filter->setReceiver(v->sink);
 
-        msg(MSG_DEBUG, "Config: sampler subsystem successfully configured");
-        return 0;
+	msg(MSG_DEBUG, "Config: sampler subsystem successfully configured");
+	return 0;
 }
 
 
@@ -140,45 +138,45 @@ int configure_sampler(struct v_objects *v)
  */
 static int configure_template(struct v_objects *v, uint16_t template_id, char *list)
 {
-        Template *t;
+	Template *t;
 	char *l, *token;
-        int tmpid;
-        const ipfix_identifier *id;
-	
-        /* violating the original string is not nice, so copy */
+	int tmpid;
+	const ipfix_identifier *id;
+
+	/* violating the original string is not nice, so copy */
 	if(!(l=strdup(list))) {
 		return 1;
 	}
-	
+
 	msg(MSG_INFO, "Template: using ID %d", template_id);
 	t=new Template(template_id);
 
-        /*
-         loop over all fields
-         accept numerical or string input
-         lookup id if string input
-         add to Template
-         */
-        while((token=strsep(&l, ","))) {
+	/*
+	 loop over all fields
+	 accept numerical or string input
+	 lookup id if string input
+	 add to Template
+	 */
+	while((token=strsep(&l, ","))) {
 
 		/*
 		 lookup field
-                 name_lookup returns -1 on error, id_lookup NULL
-                 range-check id if numerical id was given in list
+		 name_lookup returns -1 on error, id_lookup NULL
+		 range-check id if numerical id was given in list
 		 */
 		if(isalpha((int)*token)) {
 			tmpid=ipfix_name_lookup(token);
 		} else {
-                        tmpid=atoi(token);
-                        if(!ipfix_id_rangecheck(tmpid)) {
-                                msg(MSG_ERROR, "Template: ignoring template field %s -> %d - rangecheck not ok", token, tmpid);
-                                continue;
-                        }
+			tmpid=atoi(token);
+			if(!ipfix_id_rangecheck(tmpid)) {
+				msg(MSG_ERROR, "Template: ignoring template field %s -> %d - rangecheck not ok", token, tmpid);
+				continue;
+			}
 		}
 
 		if( (tmpid == -1) || ((id=ipfix_id_lookup(tmpid)) == NULL) ) {
-                        msg(MSG_ERROR, "Template: ignoring unknown template field %s", token);
-                        continue;
+			msg(MSG_ERROR, "Template: ignoring unknown template field %s", token);
+			continue;
 		}
 
 		msg(MSG_INFO, "Template: adding %s -> ID %d with size %d", token, id->id, id->length);
@@ -186,9 +184,9 @@ static int configure_template(struct v_objects *v, uint16_t template_id, char *l
 	}
 
 	free(l);
-        msg(MSG_DEBUG, "Template: got %d fields (given: %s)", t->getFieldCount(), list);
+	msg(MSG_DEBUG, "Template: got %d fields (given: %s)", t->getFieldCount(), list);
 
-        v->templ=t;
+	v->templ=t;
 
 	return 0;
 }
@@ -200,30 +198,30 @@ static int configure_template(struct v_objects *v, uint16_t template_id, char *l
  */
 static int configure_observer(struct v_objects *v, char *interface, int snaplen)
 {
-        dictionary *conf=v->v_config;
-        char *pcap_filter=iniparser_getvalue(conf, CONF_SEC, "pcap_filter");
-        Observer *o;
+	dictionary *conf=v->v_config;
+	char *pcap_filter=iniparser_getvalue(conf, CONF_SEC, "pcap_filter");
+	Observer *o;
 
-        o=new Observer(interface);
+	o=new Observer(interface);
 
-        if(!o->prepare(pcap_filter)) {
-                msg(MSG_FATAL, "Observer: preparing failed");
-                goto out;
+	if(!o->prepare(pcap_filter)) {
+		msg(MSG_FATAL, "Observer: preparing failed");
+		goto out;
 	}
 
-        if(snaplen) {
-                if(!o->setCaptureLen(snaplen)) {
+	if(snaplen) {
+		if(!o->setCaptureLen(snaplen)) {
 			msg(MSG_FATAL, "Observer: wrong snaplen specified - using %d", o->getCaptureLen());
 		}
-        }
+	}
 	msg(MSG_DEBUG, "Observer: using interface %s, snaplen %d", o->captureInterface, o->getCaptureLen());
 	v->observer=o;
 
-        return 0;
+	return 0;
 
 out:
 	delete o;
-        return 1;
+	return 1;
 }
 
 
@@ -233,57 +231,56 @@ out:
  */
 static int configure_filter(struct v_objects *v, char *list)
 {
-        char *l, *token;
+	char *l, *token;
 	char *routing;
-	
-        PacketProcessor *p, *hook_f=NULL;
+
+	PacketProcessor *p, *hook_f=NULL;
 	char *p_settings;
-	int p_id;
 
 	dictionary *conf=v->v_config;
-	
-        Filter *f=new Filter();
 
-        /* violating the original string is not nice, so copy */
-        if(!(l=strdup(list))) {
+	Filter *f=new Filter();
+
+	/* violating the original string is not nice, so copy */
+	if(!(l=strdup(list))) {
 		return 1;
-        }
+	}
 
 	/*
 	 sampler->concentrator hook
-         the HookingFilter calls the function sampler_hook_entry() from concentrator
+	 the HookingFilter calls the function sampler_hook_entry() from concentrator
 	 */
 	routing=iniparser_getvalue(conf, "main", "packets");
-        /* the concentrator should get all packet traffic */
+	/* the concentrator should get all packet traffic */
 	if(strcasecmp(routing, "all") == 0) {
 		msg(MSG_DEBUG, "Filter: adding HookingFilter sampler->concentrator as first PP");
 		hook_f=new HookingFilter(sampler_hook_entry);
 		f->addProcessor(hook_f);
 	}
 
-        /*
-         loop over all processors specified in list
-         look up setting for this processor
-         make new packetprocessor via make_filter_processor()
-         add to main filter
-         */
-        while((token=strsep(&l, ","))) {
+	/*
+	 loop over all processors specified in list
+	 look up setting for this processor
+	 make new packetprocessor via make_filter_processor()
+	 add to main filter
+	 */
+	while((token=strsep(&l, ","))) {
 
-                /* lookup setting for this particular filter in config */
+		/* lookup setting for this particular filter in config */
 		if(!(p_settings=iniparser_getvalue(conf, CONF_SEC, token))) {
 			msg(MSG_ERROR, "Filter: ignoring processor %s - has no config", token);
 			continue;
 		}
-		
-                /* make new sub-filter object */
-                if(!(p=make_filter_processor(token, p_settings))) {
-                        msg(MSG_ERROR, "Filter: ignoring processor %s(%s) - make_filter_process() error", token, p_settings);
-                        continue;
-                }
-		
+
+		/* make new sub-filter object */
+		if(!(p=make_filter_processor(token, p_settings))) {
+			msg(MSG_ERROR, "Filter: ignoring processor %s(%s) - make_filter_process() error", token, p_settings);
+			continue;
+		}
+
 		/* and add to filtering process */
-                msg(MSG_INFO, "Filter: adding processor %s<%s> to main filter", token, p_settings);
-               	f->addProcessor(p);
+		msg(MSG_INFO, "Filter: adding processor %s<%s> to main filter", token, p_settings);
+		f->addProcessor(p);
 	}
 
 	/* the concentrator should get filtered traffic */
@@ -293,14 +290,14 @@ static int configure_filter(struct v_objects *v, char *list)
 		f->addProcessor(hook_f);
 	}
 
-        /* attach the hooking filter to main struct, we need direct access */
-        v->hooking=(HookingFilter *)hook_f;
+	/* attach the hooking filter to main struct, we need direct access */
+	v->hooking=(HookingFilter *)hook_f;
 
-        v->filter=f;
+	v->filter=f;
 
-        free(l);
+	free(l);
 
-        return 0;
+	return 0;
 }
 
 /*
@@ -316,55 +313,55 @@ static int configure_filter(struct v_objects *v, char *list)
  */
 static PacketProcessor * make_filter_processor(char *name, char *setting)
 {
-        static char *PP_TAB[]={"Internal", "Systematic", "Random", "IPHeader", NULL };
+	static char *PP_TAB[]={"Internal", "Systematic", "Random", "IPHeader", NULL };
 
-        PacketProcessor *n;
+	PacketProcessor *n;
 	char *l, *token;
-        int id;
+	int id;
 
-        /*
-         the following is helluva dirty
-         keywords for grep: FUCK, DIRTY, MORON, SUCKS, SUCKZ
+	/*
+	 the following is helluva dirty
+	 keywords for grep: FUCK, DIRTY, MORON, SUCKS, SUCKZ
 
-         we don't accept filters with more than PROCESSOR_MAX_PARAM parameters
-         */
-        int p_conf[PROCESSOR_MAX_PARAM];
-        int p_conf_nr=0, i=0;
-	
-        /* really do not violate original string */
-        if(!(l=strdup(setting))) {
+	 we don't accept filters with more than PROCESSOR_MAX_PARAM parameters
+	 */
+	int p_conf[PROCESSOR_MAX_PARAM];
+	int p_conf_nr=0, i=0;
+
+	/* really do not violate original string */
+	if(!(l=strdup(setting))) {
 		return NULL;
 	}
-	
+
 	/* processor type is the first number */
 	token=strsep(&l, ",");
-        id=atoi(token);
+	id=atoi(token);
 
-        msg(MSG_DEBUG, "Filter: new PacketProcessor %s - type %d (%s), full params %s", name, id, PP_TAB[id], setting);
-        while((token=strsep(&l, ",")) && p_conf_nr < PROCESSOR_MAX_PARAM) {
-                p_conf[p_conf_nr]=atoi(token);
-                /* include hardcode debuggin in case of */
-                DPRINTF("PacketProcessor: token %s parsed as %d\n", token, p_conf[p_conf_nr]);
-                p_conf_nr++;
-        }
+	msg(MSG_DEBUG, "Filter: new PacketProcessor %s - type %d (%s), full params %s", name, id, PP_TAB[id], setting);
+	while((token=strsep(&l, ",")) && p_conf_nr < PROCESSOR_MAX_PARAM) {
+		p_conf[p_conf_nr]=atoi(token);
+		/* include hardcode debuggin in case of */
+		DPRINTF("PacketProcessor: token %s parsed as %d\n", token, p_conf[p_conf_nr]);
+		p_conf_nr++;
+	}
 
-        /* just dump the settings if one is interested */
-        while(i < p_conf_nr) {
-                msg(MSG_INFO, "Processor %s param #%d: %d", name, i, p_conf[i]);
-                i++;
-        }
+	/* just dump the settings if one is interested */
+	while(i < p_conf_nr) {
+		msg(MSG_INFO, "Processor %s param #%d: %d", name, i, p_conf[i]);
+		i++;
+	}
 
-        /*
-         the following is helluva dirty, too
-         keywords for grep: FUCK, DIRTY, MORON, SUCKS, SUCKZ
+	/*
+	 the following is helluva dirty, too
+	 keywords for grep: FUCK, DIRTY, MORON, SUCKS, SUCKZ
 
-         PROCESSOR_* are defined in config_sampler.h
-         hardcoded parameters!
-         */
-        switch(id) {
+	 PROCESSOR_* are defined in config_sampler.h
+	 hardcoded parameters!
+	 */
+	switch(id) {
 	case PROCESSOR_INTERNAL:
-                n=NULL;
-                break;
+		n=NULL;
+		break;
 	case PROCESSOR_SYSTEMATIC:
 		n=new SystematicSampler(p_conf[0], p_conf[1], p_conf[2]);
 		break;
@@ -375,13 +372,13 @@ static PacketProcessor * make_filter_processor(char *name, char *setting)
 		n=new IPHeaderFilter(p_conf[0], p_conf[1], p_conf[2], p_conf[3], p_conf[4]);
 		break;
 	default:
-                msg(MSG_FATAL, "Filter: cannot make PacketProcessor with ID %d, settings %s", id, setting);
-                n=NULL;
-        }
+		msg(MSG_FATAL, "Filter: cannot make PacketProcessor with ID %d, settings %s", id, setting);
+		n=NULL;
+	}
 out:
-        free(l);
+	free(l);
 
-        return n;
+	return n;
 }
 
 
@@ -393,55 +390,94 @@ out:
  POTENTIAL FIXME: we could have more of them, e.g. 2 exporterSinks
  will be a bit more work to figure out the Packet-free()ing then!
  */
-static int configure_sink(struct v_objects *v, char *list)
+static int configure_sink(struct v_objects *v)
 {
-        dictionary *conf;
-        Sink *s;
-        char *dst_port, *dst_ip, *source_id, *export_timeout;
+	dictionary *conf;
+	Sink *s;
 
-        conf=v->v_config;
+	conf=v->v_config;
 
-        /*
-         dont want to export ? so the sink is a simple PacketSink
-         we need a sink, or else no packets will be free()d
-         */
-        if(strcasecmp(list, "off") == 0) {
+	char *list=iniparser_getvalue(conf, CONF_SEC, "export_to");
+
+	/*
+	 dont want to export ? so the sink is a simple PacketSink
+	 we need a sink, or else no packets will be free()d
+	 */
+	if(strcasecmp(list, "off") == 0) {
 		msg(MSG_DEBUG, "Sink: making plain PacketSink()");
 		s=new PacketSink();
-        } else {
-		int sID, timeout;
-                ExporterSink *e;
+	} else {
 
-		source_id=iniparser_getvalue(conf, CONF_SEC, "source_id");
-		dst_port=iniparser_getvalue(conf, CONF_SEC, "export_port");
-		dst_ip=iniparser_getvalue(conf, CONF_SEC, "export_ip");
+		ExporterSink *e;
+		int sID, timeout, exports=0;
+
+		char *source_id, *export_timeout;
+
+		char *l, *token=NULL;
+		uri **export_uri=v->samp_uris;
+
 		export_timeout=iniparser_getvalue(conf, CONF_SEC, "export_timeout");
+		source_id=iniparser_getvalue(conf, CONF_SEC, "source_id");
+
+		/* violating the original string is not nice, so copy */
+		if(!(l=strdup(iniparser_getvalue(conf, CONF_SEC, "export_to")))) {
+			return 1;
+		}
 
 		/* check parameters */
-		if(!source_id || !dst_port || !dst_ip || !export_timeout) {
-			msg(MSG_FATAL,
-			    "Sink: not all parameters in config: source_id: %s, dst_port: %s, dst_ip: %s, export_timeout: %s",
-			    source_id, dst_port, dst_ip, export_timeout
-			   );
+		if(!source_id || !export_timeout) {
+			msg(MSG_FATAL, "Sink: not all parameters in config");
 			return 1;
 		}
 
 		sID=atoi(source_id);
-                timeout=atoi(export_timeout);
+		timeout=atoi(export_timeout);
+
+		/*
+		 loop over all given URIs
+		 parse URI
+		 */
+		DPRINTF("ExporterSink URIs: %s\n", l);
+		while((token=strsep(&l, ",")) && exports < MAX_URIS) {
+			uri *u;
+
+			DPRINTF("Trying to parse %s into URI\n", token);
+			u=uri_parse(token);
+			DPRINTF("Parsed %s into proto: %s, IP: %s, port: %d\n", token, u->scheme, u->host, u->port);
+
+			export_uri[exports]=u;
+			exports++;
+		}
+		exports--;
+
+		free(l);
+
 		/* do a real exporter sink */
-		msg(MSG_DEBUG, "Sink: making ExporterSink(), own source_ID %d, dst %s:%s, timeout %d ms",
-		    sID,
-                    dst_ip,
-		    dst_port,
-		    timeout
+		msg(MSG_DEBUG, "Sink: making ExporterSink(), own source_ID %d, timeout %d ms",
+		    sID, timeout
 		   );
 		e=new ExporterSink(v->templ, sID);
-		e->addCollector(dst_ip, (uint16_t)atoi(dst_port), "UDP");
-                e->setExportTimeout(timeout);
-                s=e;
-        }
 
-        v->sink=s;
+		/*
+		 add all receiving collectors here
+		 >= 0 because we need to add all
+		 */
+		while(exports >= 0) {
+			msg(MSG_DEBUG, "Config: adding collector %s:%d to ExporterSink",
+			    export_uri[exports]->host, export_uri[exports]->port
+			   );
+			e->addCollector(export_uri[exports]->host,
+					export_uri[exports]->port,
+					export_uri[exports]->scheme
+				       );
+			exports--;
+		}
 
-        return 0;
+		e->setExportTimeout(timeout);
+		s=e;
+	}
+
+	v->sink=s;
+
+	return 0;
 }
