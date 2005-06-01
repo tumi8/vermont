cc9cf4a51e5873785cf77c7a1feea1d8c0328279
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-06-01 13:28:13 +0000

    -Add conc_uris[] and samp_uris[] to vermont struct if one wants to free them at shutdown
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@332 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- vermont.h
+++ vermont.h
@@ -4,6 +4,8 @@
 #include <vector>
 
 #include "iniparser.h"
+#include "uri.h"
+
 #include "sampler/Template.h"
 #include "sampler/Observer.h"
 #include "sampler/Filter.h"
@@ -15,6 +17,7 @@
 #include "concentrator/aggregator.h"
 #include "concentrator/sndIpfix.h"
 
+#define MAX_URIS 16
 
 /* holding all objects/handles/... for the subsystems like sampler and collector */
 struct v_objects {
@@ -27,22 +30,28 @@ struct v_objects {
         /* starting time */
         time_t v_starttime;
 
-	/* for sampler: */
+
+        /* for sampler: */
 	Template *templ;
 	Observer *observer;
         Filter *filter;
         Sink *sink;
 
-	/* and this is pragmatic */
+        /* and this is pragmatic - direct access to hooking filter */
         HookingFilter *hooking;
 
-	/* for concentrator: */
+        uri *samp_uris[MAX_URIS];
+
+
+        /* for concentrator: */
         IpfixReceiver *conc_receiver;
         IpfixAggregator *conc_aggregator;
         IpfixSender *conc_exporter;
 
         /* poll aggregator this often */
         unsigned short conc_poll_ms;
+
+        uri *conc_uris[MAX_URIS];
 
         int conc_exitflag;
 
