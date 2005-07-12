d0d5504c4ac55ab5f7e83362d2ff404d5c6a1986
freequaos <freequaos@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2005-07-12 08:18:13 +0000

    -Class header cleanups for Observer o remove big and no speed-critical prepare() into .cpp so compiler will not even think of inlining
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/trunk/vermont@405 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/Observer.h
+++ sampler/Observer.h
@@ -71,80 +71,6 @@ public:
         };
 
 	/*
-	 call after an Observer has been created
-         error checking on pcap here, because it can't be done in the constructor
-         and it may be too late, if done in the thread
-         */
-	bool prepare(char *filter_exp)
-	{
-                struct in_addr i_netmask, i_network;
-
-                // query all available capture devices
-		msg(MSG_INFO, "Observer: Finding devices");
-		if(pcap_findalldevs(&allDevices, errorBuffer) == -1) {
-			msg(MSG_FATAL, "Observer: error getting list of interfaces: %s", errorBuffer);
-			goto out;
-		}
-
-		for(pcap_if_t *dev = allDevices; dev != NULL; dev=dev->next) {
-			msg(MSG_DEBUG, "PCAP: name=%s, desc=%s", dev->name, dev->description);
-                }
-
-                msg(MSG_INFO,
-                    "Observer: pcap opening interface=%s, promisc=%d, snaplen=%d, timeout=%d",
-                    captureInterface, pcap_promisc, capturelen, pcap_timeout
-                   );
-                captureDevice=pcap_open_live(captureInterface, capturelen, pcap_promisc, pcap_timeout, errorBuffer);
-                // check for errors
-                if(!captureDevice) {
-                        msg(MSG_FATAL, "Observer: Error initializing pcap interface: %s", errorBuffer);
-                        goto out1;
-                }
-
-                /* we need the netmask for the pcap_compile */
-                if(pcap_lookupnet(captureInterface, &network, &netmask, errorBuffer) == -1) {
-                        msg(MSG_FATAL, "Observer: unable to determine netmask/network: %s", errorBuffer);
-                        goto out2;
-                }
-                i_network.s_addr=network;
-                i_netmask.s_addr=netmask;
-                msg(MSG_DEBUG, "Observer: pcap seems to run on network %s", inet_ntoa(i_network));
-                msg(MSG_INFO, "Observer: pcap seems to run on netmask %s", inet_ntoa(i_netmask));
-
-                if(filter_exp) {
-                        msg(MSG_DEBUG, "Observer: compiling pcap filter code from: %s", filter_exp);
-                        if(pcap_compile(captureDevice, &pcap_filter, filter_exp, 1, netmask) == -1) {
-                                msg(MSG_FATAL, "Observer: unable to validate+compile pcap filter");
-                                goto out2;
-                        }
-
-                        if(pcap_setfilter(captureDevice, &pcap_filter) == -1) {
-                                msg(MSG_FATAL, "Observer: unable to attach filter to pcap: %s", pcap_geterr(captureDevice));
-                                goto out3;
-                        }
-                        /* you may free an attached code, see man-page */
-                        pcap_freecode(&pcap_filter);
-                } else {
-                        msg(MSG_DEBUG, "Observer: using no pcap filter");
-                }
-
-                ready=true;
-
-                return true;
-
-        out3:
-                pcap_freecode(&pcap_filter);
-        out2:
-		pcap_close(captureDevice);
-                captureDevice=NULL;
-        out1:
-		pcap_freealldevs(allDevices);
-                allDevices=NULL;
-        out:
-                return false;
-        }
-
-	/*
 	 call to get the main capture thread running
          open() has to be called before
 	 */
@@ -207,9 +133,6 @@ public:
                 return pcap_timeout;
         }
 
-	/* this function is called by the logger timer thread and should dump some nice info using msg_stat */
-	static void doLogging(void *arg);
-
         /*
          get some capturing statistics
          struct pcap_stat is defined in pcap.h and has at least 3 u_int variables:
@@ -221,6 +144,10 @@ public:
         {
                 return(pcap_stats(captureDevice, out));
         }
+
+
+        bool prepare(char *filter_exp);
+	static void doLogging(void *arg);
 
 
 protected:
