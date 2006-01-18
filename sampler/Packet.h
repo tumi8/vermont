f31a6e0fa5e921bd550cbb3d30cb4c8096b6eefb
doc_wagon <doc_wagon@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2006-01-18 23:08:18 +0000

    - renamed ipHeader to netHeader - added initial packet classification by content (IP/TCP/UDP/... header   detection)
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@517 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/Packet.h
+++ sampler/Packet.h
@@ -29,10 +29,41 @@
 
 #include "Lock.h"
 
-// the various header types
+// the various header types (actually, HEAD_PAYLOAD is not neccessarily a header but it works like one for
+// our purposes)
 #define HEAD_RAW        0
 #define HEAD_NETWORK    1
 #define HEAD_TRANSPORT  2
+#define HEAD_PAYLOAD    3
+
+// Packet classifications
+//
+// This bitmask defines various classifications for the packet. Which protocol headers it contains for example
+// Bits:
+//   3                   2                   1  
+// 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+// |               |~~~~~~~~~~~~~~~~~~~~~| |~~~~~~~~~~~~~~~~~~~~~|
+// |               |                     | +---------------------+----- Network headers
+// |               +---------------------+----------------------------- Transport headers
+// +------------------------------------------------------------------- Payload available
+
+// Network header classifications
+#define PCLASS_NET_IP4             (1UL <<  0)
+#define PCLASS_NET_IV6             (1UL <<  1)
+
+#define PCLASS_NETMASK             0x00000fff
+
+// Transport header classifications
+#define PCLASS_TRN_TCP             (1UL << 12)
+#define PCLASS_TRN_UDP             (1UL << 13)
+#define PCLASS_TRN_ICMP            (1UL << 14)
+#define PCLASS_TRN_IGMP            (1UL << 15)
+
+#define PCLASS_TRNMASK             0x00fff000
+// .... etc. etc. etc. 
+
+// Payload classification
+#define PCLASS_PAYLOAD             (1UL << 31)
 
 class Packet
 {
@@ -47,8 +78,12 @@ public:
 	 transportHeader: start of the transport layer header (TCP/UDP): ip_header + variable IP header length
 	 */
 	void *data;
-	void *ipHeader;
+	void *netHeader;
 	void *transportHeader;
+	void *payload;
+
+	// the packet classification, i.e. what headers are present?
+	unsigned long classification;
 
 	// The length of the packet in bytes
 	unsigned int length;
@@ -60,12 +95,13 @@ public:
 	Packet(void *packetData, unsigned int len, int numUsers = 1) : users(numUsers), refCountLock()
 	{
 		data = packetData;
-		ipHeader = (unsigned char *)data + IPHeaderOffset;
-		transportHeader = (unsigned char *)ipHeader + ipTransportHeaderOffset(ipHeader);
+		netHeader = (unsigned char *)data + IPHeaderOffset;
+		//transportHeader = (unsigned char *)netHeader + netTransportHeaderOffset(netHeader);
 		length = len;
 
 		totalPacketsReceived++;
 
+		classify();
 		/*
 		 DO NOT SET TIMESTAMP HERE
 		 IS SET IN OBSERVER!
@@ -98,13 +134,80 @@ public:
 		} else if(newUsers < 0) {
 			DPRINTF("Packet: WARNING: trying to free already freed packet!\n");
 		}
-
 	};
+
+	// classify the packet headers
+	void classify()
+	{
+		unsigned char protocol = 0;
+		unsigned char tcpDataOffset;
+		
+		classification = 0;
+		transportHeader = 0;
+		payload = 0;
+		
+		// first check for IPv4 header
+		if ( (*((unsigned char *)netHeader) >> 4) == 4)
+		{
+			classification |= PCLASS_NET_IP4;
+			transportHeader = (unsigned char *)netHeader + ( ( *((unsigned char *)netHeader) & 0x0f ) << 2);
+			protocol = *((unsigned char *)netHeader + 9);
+		}
+
+		// if we found a transport header, continue classifying
+		if (transportHeader && protocol)
+		{
+			switch (protocol)
+			{
+			case 1:		// ICMP
+				classification |= PCLASS_TRN_ICMP;
+
+				// ICMP header is 4 bytes fixed-length
+				payload = (unsigned char *)transportHeader + 4;
+				
+				break;
+			case 2:		// IGMP
+				classification |= PCLASS_TRN_IGMP;
+
+				// header is 8-bytes fixed size
+				payload = (unsigned char *)transportHeader + 8;
+				
+				break;
+			case 6:         // TCP
+				classification |= PCLASS_TRN_TCP;
+				
+				// extract "Data Offset" field at TCP header offset 12 (upper 4 bits)
+				tcpDataOffset = *((unsigned char *)transportHeader + 12) >> 4;
+				
+				// calculate payload offset
+				payload = (unsigned char *)transportHeader + (tcpDataOffset << 2);
+				
+				break;
+				
+			case 17:        // UDP
+				classification |= PCLASS_TRN_UDP;
+				// UDP has a fixed header size of 8 bytes
+
+				payload = (unsigned char *)transportHeader + 8;
+
+				break;
+				
+			default:
+				break;
+			}
+
+			// check if we actually _have_ payload
+			if (payload && ((char *)payload < (char *)data + length))
+				classification |= PCLASS_PAYLOAD;
+
+			//fprintf(stderr, "proto %d, data %p, net %p, trn %p, payload %p\n", protocol, data, netHeader, transportHeader, data);
+		}
+	}
 
 	// read data from the IP header
 	void copyPacketData(void *dest, int offset, int size) const
 	{
-		memcpy(dest, (char *)ipHeader + offset, size);
+		memcpy(dest, (char *)netHeader + offset, size);
 	}
 
 	// return a pointer into the packet to IP header offset given
@@ -115,9 +218,12 @@ public:
 		case HEAD_RAW:
 			return (char *)data + offset;
 		case HEAD_NETWORK:
-			return (char *)ipHeader + offset;
+			return (char *)netHeader + offset;
 		case HEAD_TRANSPORT:
 			return (char *)transportHeader + offset;
+// TODO: need to check with packet length, too			
+//		case HEAD_PAYLOAD:
+//			return (char *)payload + offset;
 		default:
 			return (char *)data + offset;
 		}
@@ -142,7 +248,7 @@ private:
 	 return the offset the transport header lies
 	 IP knows about variable length options field
          */
-	static inline unsigned int ipTransportHeaderOffset(void *ipPacket)
+	static inline unsigned int netTransportHeaderOffset(void *ipPacket)
 	{
 		/*
 		 the header length (incl. options field) is:
