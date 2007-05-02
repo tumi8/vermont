d43dd084f5c7b6d2c8886916d58080d8a8224831
sommer <sommer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-05-02 20:28:29 +0000

    HookingFilter now owns the Templates it uses
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@861 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sampler/HookingFilter.cpp
+++ sampler/HookingFilter.cpp
@@ -13,12 +13,97 @@
 
 #include "msg.h"
 
+
+namespace {
+	void setFieldInfo(IpfixRecord::FieldInfo* fi, IpfixRecord::FieldInfo::Type::Id id, IpfixRecord::FieldInfo::Type::Length length, uint16_t offset) {
+		fi->type.id = id;
+		fi->type.length = length;
+		fi->type.isVariableLength = 0;
+		fi->type.eid = 0;
+		fi->offset = offset; 
+	}
+}
+
+HookingFilter::HookingFilter(FlowSink *flowSink) : flowSink(flowSink) {
+	ip_traffic_fi = (IpfixRecord::FieldInfo*)malloc(7 * sizeof(IpfixRecord::FieldInfo));
+	setFieldInfo(&ip_traffic_fi[0], IPFIX_TYPEID_packetDeltaCount, 1, 10);
+	setFieldInfo(&ip_traffic_fi[1], IPFIX_TYPEID_flowStartSeconds, 4, 4);
+	setFieldInfo(&ip_traffic_fi[2], IPFIX_TYPEID_flowEndSeconds, 4, 4);
+	setFieldInfo(&ip_traffic_fi[3], IPFIX_TYPEID_octetDeltaCount, 2, 2);
+	setFieldInfo(&ip_traffic_fi[4], IPFIX_TYPEID_protocolIdentifier, 1, 9);
+	setFieldInfo(&ip_traffic_fi[5], IPFIX_TYPEID_sourceIPv4Address, 4, 12);
+	setFieldInfo(&ip_traffic_fi[6], IPFIX_TYPEID_destinationIPv4Address, 4, 16);
+
+	icmp_traffic_fi = (IpfixRecord::FieldInfo*)malloc(8 * sizeof(IpfixRecord::FieldInfo));
+	setFieldInfo(&icmp_traffic_fi[0], IPFIX_TYPEID_icmpTypeCode, 2, 0);
+	setFieldInfo(&icmp_traffic_fi[1], IPFIX_TYPEID_packetDeltaCount, 1, 10);
+	setFieldInfo(&icmp_traffic_fi[2], IPFIX_TYPEID_flowStartSeconds, 4, 4);
+	setFieldInfo(&icmp_traffic_fi[3], IPFIX_TYPEID_flowEndSeconds, 4, 4);
+	setFieldInfo(&icmp_traffic_fi[4], IPFIX_TYPEID_octetDeltaCount, 2, 2);
+	setFieldInfo(&icmp_traffic_fi[5], IPFIX_TYPEID_protocolIdentifier, 1, 9);
+	setFieldInfo(&icmp_traffic_fi[6], IPFIX_TYPEID_sourceIPv4Address, 4, 12);
+	setFieldInfo(&icmp_traffic_fi[7], IPFIX_TYPEID_destinationIPv4Address, 4, 16);
+
+	udp_traffic_fi = (IpfixRecord::FieldInfo*)malloc(9 * sizeof(IpfixRecord::FieldInfo));
+	setFieldInfo(&udp_traffic_fi[0], IPFIX_TYPEID_sourceTransportPort, 2, 0);
+	setFieldInfo(&udp_traffic_fi[1], IPFIX_TYPEID_destinationTransportPort, 2, 2);
+	setFieldInfo(&udp_traffic_fi[2], IPFIX_TYPEID_packetDeltaCount, 1, 10);
+	setFieldInfo(&udp_traffic_fi[3], IPFIX_TYPEID_flowStartSeconds, 4, 4);
+	setFieldInfo(&udp_traffic_fi[4], IPFIX_TYPEID_flowEndSeconds, 4, 4);
+	setFieldInfo(&udp_traffic_fi[5], IPFIX_TYPEID_octetDeltaCount, 2, 2);
+	setFieldInfo(&udp_traffic_fi[6], IPFIX_TYPEID_protocolIdentifier, 1, 9);
+	setFieldInfo(&udp_traffic_fi[7], IPFIX_TYPEID_sourceIPv4Address, 4, 12);
+	setFieldInfo(&udp_traffic_fi[8], IPFIX_TYPEID_destinationIPv4Address, 4, 16);
+
+	tcp_traffic_fi = (IpfixRecord::FieldInfo*)malloc(10 * sizeof(IpfixRecord::FieldInfo));
+	setFieldInfo(&tcp_traffic_fi[0], IPFIX_TYPEID_tcpControlBits, 1, 13);
+	setFieldInfo(&tcp_traffic_fi[1], IPFIX_TYPEID_sourceTransportPort, 2, 0);
+	setFieldInfo(&tcp_traffic_fi[2], IPFIX_TYPEID_destinationTransportPort, 2, 2);
+	setFieldInfo(&tcp_traffic_fi[3], IPFIX_TYPEID_packetDeltaCount, 1, 10);
+	setFieldInfo(&tcp_traffic_fi[4], IPFIX_TYPEID_flowStartSeconds, 4, 4);
+	setFieldInfo(&tcp_traffic_fi[5], IPFIX_TYPEID_flowEndSeconds, 4, 4);
+	setFieldInfo(&tcp_traffic_fi[6], IPFIX_TYPEID_octetDeltaCount, 2, 2);
+	setFieldInfo(&tcp_traffic_fi[7], IPFIX_TYPEID_protocolIdentifier, 1, 9);
+	setFieldInfo(&tcp_traffic_fi[8], IPFIX_TYPEID_sourceIPv4Address, 4, 12);
+	setFieldInfo(&tcp_traffic_fi[9], IPFIX_TYPEID_destinationIPv4Address, 4, 16);
+
+	ip_traffic_template = new IpfixRecord::TemplateInfo();
+	ip_traffic_template->templateId = 0;
+	ip_traffic_template->fieldCount = 7;
+	ip_traffic_template->fieldInfo  = ip_traffic_fi;
+	ip_traffic_template->userData   = 0;
+
+	icmp_traffic_template = new IpfixRecord::TemplateInfo();
+	icmp_traffic_template->templateId = 0;
+	icmp_traffic_template->fieldCount = 8;
+	icmp_traffic_template->fieldInfo  = icmp_traffic_fi;
+	icmp_traffic_template->userData   = 0;
+
+	udp_traffic_template = new IpfixRecord::TemplateInfo();
+	udp_traffic_template->templateId = 0;
+	udp_traffic_template->fieldCount = 9;
+	udp_traffic_template->fieldInfo  = udp_traffic_fi;
+	udp_traffic_template->userData   = 0;
+
+	tcp_traffic_template = new IpfixRecord::TemplateInfo();
+	tcp_traffic_template->templateId = 0;
+	tcp_traffic_template->fieldCount = 10;
+	tcp_traffic_template->fieldInfo  = tcp_traffic_fi;
+	tcp_traffic_template->userData   = 0; 
+}
+
+HookingFilter::~HookingFilter() {
+	delete ip_traffic_template;
+	delete icmp_traffic_template;
+	delete udp_traffic_template;
+	delete tcp_traffic_template;
+}
+
 /*
  this is the function called by sampler::HookingFilter
  the entrypoint into the concentrator
  this is a mess and not thread safe
  */
-
 bool HookingFilter::processPacket(const Packet *p)
 {
 	/* we want only IPv4 packets  */
@@ -40,7 +125,7 @@ bool HookingFilter::processPacket(const Packet *p)
 
 	// Check if transport header is available
 	if(p->transportHeader == NULL) {
-	    flowSink->onDataRecord(NULL, &ip_traffic_template, p->data_length, fdata);
+	    flowSink->onDataRecord(NULL, ip_traffic_template, p->data_length, fdata);
 	}
 	else
 	{
@@ -54,10 +139,10 @@ bool HookingFilter::processPacket(const Packet *p)
 		       - use this offset and add to src/dst_port offset
 		     */
 		    transport_offset=abs((char*)p->transportHeader - (char*)p->netHeader);
-		    icmp_traffic_template.fieldInfo[0].offset += transport_offset;
-		    flowSink->onDataRecord(NULL, &icmp_traffic_template, p->data_length, fdata);
+		    icmp_traffic_template->fieldInfo[0].offset += transport_offset;
+		    flowSink->onDataRecord(NULL, icmp_traffic_template, p->data_length, fdata);
 		    /* reset offset for typecode to starting value */
-		    icmp_traffic_template.fieldInfo[0].offset = 0;
+		    icmp_traffic_template->fieldInfo[0].offset = 0;
 		    break;
 		case IPFIX_protocolIdentifier_UDP:
 		    /*
@@ -67,12 +152,12 @@ bool HookingFilter::processPacket(const Packet *p)
 		       - use this offset and add to src/dst_port offset
 		     */
 		    transport_offset=abs((char*)p->transportHeader - (char*)p->netHeader);
-		    udp_traffic_template.fieldInfo[0].offset += transport_offset;
-		    udp_traffic_template.fieldInfo[1].offset += transport_offset;
-		    flowSink->onDataRecord(NULL, &udp_traffic_template, p->data_length, fdata);
+		    udp_traffic_template->fieldInfo[0].offset += transport_offset;
+		    udp_traffic_template->fieldInfo[1].offset += transport_offset;
+		    flowSink->onDataRecord(NULL, udp_traffic_template, p->data_length, fdata);
 		    /* reset offsets for srcport/dstport to starting values */
-		    udp_traffic_template.fieldInfo[0].offset = 0;
-		    udp_traffic_template.fieldInfo[1].offset = 2;
+		    udp_traffic_template->fieldInfo[0].offset = 0;
+		    udp_traffic_template->fieldInfo[1].offset = 2;
 		    break;
 		case IPFIX_protocolIdentifier_TCP:
 		    /*
@@ -82,17 +167,17 @@ bool HookingFilter::processPacket(const Packet *p)
 		       - use this offset and add to src/dst_port offset
 		     */
 		    transport_offset=abs((char*)p->transportHeader - (char*)p->netHeader);
-		    tcp_traffic_template.fieldInfo[0].offset += transport_offset;
-		    tcp_traffic_template.fieldInfo[1].offset += transport_offset;
-		    tcp_traffic_template.fieldInfo[2].offset += transport_offset;
-		    flowSink->onDataRecord(NULL, &tcp_traffic_template, p->data_length, fdata);
+		    tcp_traffic_template->fieldInfo[0].offset += transport_offset;
+		    tcp_traffic_template->fieldInfo[1].offset += transport_offset;
+		    tcp_traffic_template->fieldInfo[2].offset += transport_offset;
+		    flowSink->onDataRecord(NULL, tcp_traffic_template, p->data_length, fdata);
 		    /* reset offsets for srcport/dstport to starting values */
-		    tcp_traffic_template.fieldInfo[0].offset = 13;
-		    tcp_traffic_template.fieldInfo[1].offset = 0;
-		    tcp_traffic_template.fieldInfo[2].offset = 2;
+		    tcp_traffic_template->fieldInfo[0].offset = 13;
+		    tcp_traffic_template->fieldInfo[1].offset = 0;
+		    tcp_traffic_template->fieldInfo[2].offset = 2;
 		    break;
 		default:
-		    flowSink->onDataRecord(NULL, &ip_traffic_template, p->data_length, fdata);
+		    flowSink->onDataRecord(NULL, ip_traffic_template, p->data_length, fdata);
 	    }
 	}
 
@@ -102,83 +187,5 @@ bool HookingFilter::processPacket(const Packet *p)
 
 	return true;
 }
-
-IpfixRecord::FieldInfo HookingFilter::ip_traffic_fi[] = {
-	/* { { ID, len, enterprise}, offset} */
-	{ {IPFIX_TYPEID_packetDeltaCount,         1, 0}, 10},
-	{ {IPFIX_TYPEID_flowStartSeconds,         4, 0}, 4},
-	{ {IPFIX_TYPEID_flowEndSeconds,           4, 0}, 4},
-	{ {IPFIX_TYPEID_octetDeltaCount,          2, 0}, 2},
-	{ {IPFIX_TYPEID_protocolIdentifier,       1, 0}, 9},
-	{ {IPFIX_TYPEID_sourceIPv4Address,        4, 0}, 12},
-	{ {IPFIX_TYPEID_destinationIPv4Address,   4, 0}, 16}
-};
-
-IpfixRecord::FieldInfo HookingFilter::icmp_traffic_fi[] = {
-	/* { { ID, len, enterprise}, offset} */
-	{ {IPFIX_TYPEID_icmpTypeCode,             2, 0}, 0},
-	{ {IPFIX_TYPEID_packetDeltaCount,         1, 0}, 10},
-	{ {IPFIX_TYPEID_flowStartSeconds,         4, 0}, 4},
-	{ {IPFIX_TYPEID_flowEndSeconds,           4, 0}, 4},
-	{ {IPFIX_TYPEID_octetDeltaCount,          2, 0}, 2},
-	{ {IPFIX_TYPEID_protocolIdentifier,       1, 0}, 9},
-	{ {IPFIX_TYPEID_sourceIPv4Address,        4, 0}, 12},
-	{ {IPFIX_TYPEID_destinationIPv4Address,   4, 0}, 16}
-};
-
-IpfixRecord::FieldInfo HookingFilter::udp_traffic_fi[] = {
-	/* { { ID, len, enterprise}, offset} */
-	{ {IPFIX_TYPEID_sourceTransportPort,      2, 0}, 0},
-	{ {IPFIX_TYPEID_destinationTransportPort, 2, 0}, 2},
-	{ {IPFIX_TYPEID_packetDeltaCount,         1, 0}, 10},
-	{ {IPFIX_TYPEID_flowStartSeconds,         4, 0}, 4},
-	{ {IPFIX_TYPEID_flowEndSeconds,           4, 0}, 4},
-	{ {IPFIX_TYPEID_octetDeltaCount,          2, 0}, 2},
-	{ {IPFIX_TYPEID_protocolIdentifier,       1, 0}, 9},
-	{ {IPFIX_TYPEID_sourceIPv4Address,        4, 0}, 12},
-	{ {IPFIX_TYPEID_destinationIPv4Address,   4, 0}, 16}
-};
-
-IpfixRecord::FieldInfo HookingFilter::tcp_traffic_fi[] = {
-	/* { { ID, len, enterprise}, offset} */
-	{ {IPFIX_TYPEID_tcpControlBits,           1, 0}, 13},
-	{ {IPFIX_TYPEID_sourceTransportPort,      2, 0}, 0},
-	{ {IPFIX_TYPEID_destinationTransportPort, 2, 0}, 2},
-	{ {IPFIX_TYPEID_packetDeltaCount,         1, 0}, 10},
-	{ {IPFIX_TYPEID_flowStartSeconds,         4, 0}, 4},
-	{ {IPFIX_TYPEID_flowEndSeconds,           4, 0}, 4},
-	{ {IPFIX_TYPEID_octetDeltaCount,          2, 0}, 2},
-	{ {IPFIX_TYPEID_protocolIdentifier,       1, 0}, 9},
-	{ {IPFIX_TYPEID_sourceIPv4Address,        4, 0}, 12},
-	{ {IPFIX_TYPEID_destinationIPv4Address,   4, 0}, 16}
-};
-
-IpfixRecord::TemplateInfo HookingFilter::ip_traffic_template = {
-	/*.templateId =*/ 0,
-	/*.fieldCount =*/ 7,
-	/*.fieldInfo  =*/ ip_traffic_fi,
-	/*.userData   =*/ NULL
-};
-
-IpfixRecord::TemplateInfo HookingFilter::icmp_traffic_template = {
-	/*.templateId =*/ 0,
-	/*.fieldCount =*/ 8,
-	/*.fieldInfo  =*/ icmp_traffic_fi,
-	/*.userData   =*/ NULL
-};
-
-IpfixRecord::TemplateInfo HookingFilter::udp_traffic_template = {
-	/*.templateId =*/ 0,
-	/*.fieldCount =*/ 9,
-	/*.fieldInfo  =*/ udp_traffic_fi,
-	/*.userData   =*/ NULL
-};
-
-IpfixRecord::TemplateInfo HookingFilter::tcp_traffic_template = {
-	/*.templateId =*/ 0,
-	/*.fieldCount =*/ 10,
-	/*.fieldInfo  =*/ tcp_traffic_fi,
-	/*.userData   =*/ NULL
-};
 
 
