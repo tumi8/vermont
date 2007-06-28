bc071e88a061685ca1caba8a54032778805d520d
sommer <sommer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-06-28 10:59:31 +0000

    Tests for parser stability and rawdir reader/writer
    git-svn-id: file:///Users/braun/svn/vermont/trunk/vermont@956 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- tests/test_concentrator.cpp
+++ tests/test_concentrator.cpp
@@ -7,29 +7,68 @@
 #include "concentrator/IpfixRecord.hpp"
 #include "concentrator/FlowSource.hpp"
 #include "concentrator/FlowSink.hpp"
+#include "ipfixlolib/ipfixlolib.h"
+#include "concentrator/IpfixSender.hpp"
+#include "concentrator/IpfixRawdirReader.hpp"
+#include "concentrator/IpfixRawdirWriter.hpp"
+#include "concentrator/IpfixCollector.hpp"
+#include "concentrator/IpfixPrinter.hpp"
 
 class TestSink : public FlowSink {
 	public:
-		int onTemplate(IpfixRecord::SourceID* sourceID, const IpfixRecord::TemplateInfo* templateInfo) 
-		{
+		int receivedRecords;
+		bool checkSourceId;
+
+		TestSink(bool checkSourceId = true) : receivedRecords(0), checkSourceId(checkSourceId) {
+		}
+
+		int onTemplate(IpfixRecord::SourceID* sourceID, IpfixRecord::TemplateInfo* templateInfo) {
 			return 0;
 		}
 
-		int onDataRecord(IpfixRecord::SourceID* sourceID, const IpfixRecord::TemplateInfo* templateInfo, uint16_t length, IpfixRecord::Data* data) {
+		int onDataRecord(IpfixRecord::SourceID* sourceID, IpfixRecord::TemplateInfo* templateInfo, uint16_t length, IpfixRecord::Data* data) {
+			receivedRecords++;
+
 			uint8_t inSourceID = sourceID->exporterAddress.ip[0];
-			uint8_t inTemplateId = templateInfo->templateId;
+			uint8_t inTemplateId = templateInfo->templateId - 256;
 			uint8_t inTypeId = templateInfo->fieldInfo[0].type.id;
 			uint8_t inData = data[0];
-			if (inSourceID != inTemplateId) BOOST_ERROR("SourceID or TemplateInfo got corrupted: inSourceID != inTemplateId");
+
+			msg(MSG_DEBUG, "Received DataRecord: %d, %d, %d, %d", inSourceID, inTemplateId, inTypeId, inData);
+			if (checkSourceId) if (inSourceID != inTemplateId) BOOST_ERROR("SourceID or TemplateInfo got corrupted: inSourceID != inTemplateId");
 			if (inTemplateId != inTypeId) BOOST_ERROR("TemplateInfo got corrupted: inTemplateId != inTypeId");
 			if (inData != inTemplateId) BOOST_ERROR("IpfixRecord got corrupted: inData != inTemplateId");
 			if (inData != inTypeId) BOOST_ERROR("IpfixRecord got corrupted: inData != inTypeId");
+
 			for (int i = 0; i < rand(); i++);
+
 			return 0;
 		}
 
-		int onTemplateDestruction(IpfixRecord::SourceID* sourceID, const IpfixRecord::TemplateInfo* templateInfo) 
-		{
+		int onDataDataRecord(IpfixRecord::SourceID* sourceID, IpfixRecord::DataTemplateInfo* dataTemplateInfo, uint16_t length, IpfixRecord::Data* data) {
+			receivedRecords++;
+
+			uint8_t inSourceID = sourceID->exporterAddress.ip[0];
+			uint8_t inTemplateId = dataTemplateInfo->templateId - 256;
+			uint8_t inTypeId = dataTemplateInfo->fieldInfo[0].type.id;
+			uint8_t inData = data[0];
+			uint8_t inDataTemplateTypeId = dataTemplateInfo->dataInfo[0].type.id;
+			uint8_t inDataTemplate = dataTemplateInfo->data[0];
+			msg(MSG_DEBUG, "Received DataDataRecord: %d, %d, %d, %d, %d, %d", inSourceID, inTemplateId, inTypeId, inData, inDataTemplateTypeId, inDataTemplate);
+			if (checkSourceId) if (inSourceID != inTemplateId) BOOST_ERROR("SourceID or TemplateInfo got corrupted: inSourceID != inTemplateId");
+			if (inTemplateId != inTypeId) BOOST_ERROR("TemplateInfo got corrupted: inTemplateId != inTypeId");
+			if (inData != inTemplateId) BOOST_ERROR("IpfixRecord got corrupted: inData != inTemplateId");
+			if (inData != inTypeId) BOOST_ERROR("IpfixRecord got corrupted: inData != inTypeId");
+			if (inData != inDataTemplateTypeId) BOOST_ERROR("IpfixRecord got corrupted: inData != inDataTemplateTypeId");
+			if (inData != inDataTemplate) BOOST_ERROR("IpfixRecord got corrupted: inData != inDataTemplate");
+
+			for (int i = 0; i < rand(); i++);
+
+			return 0;
+		}
+
+
+		int onTemplateDestruction(IpfixRecord::SourceID* sourceID, IpfixRecord::TemplateInfo* templateInfo) {
 			return 0;
 		}
 
@@ -39,9 +78,16 @@ class TestSink : public FlowSink {
 
 };
 
+
+boost::shared_array<uint8_t> createTestData(uint8_t magic_number) {
+		boost::shared_array<uint8_t> testData(new uint8_t[1]);
+		testData.get()[0] = magic_number;
+		return testData;
+}
+
 boost::shared_ptr<IpfixRecord::TemplateInfo> createTestTemplate(uint8_t magic_number) {
 		boost::shared_ptr<IpfixRecord::TemplateInfo> testTemplate(new IpfixRecord::TemplateInfo);
-		testTemplate->templateId = magic_number;
+		testTemplate->templateId = magic_number + 256;
 		testTemplate->fieldCount = 1;
 		testTemplate->fieldInfo = (IpfixRecord::FieldInfo*)malloc(testTemplate->fieldCount * sizeof(IpfixRecord::FieldInfo));
 		testTemplate->fieldInfo[0].type.id = magic_number;
@@ -53,28 +99,86 @@ boost::shared_ptr<IpfixRecord::TemplateInfo> createTestTemplate(uint8_t magic_nu
 		return testTemplate;
 }
 
+boost::shared_ptr<IpfixRecord::DataTemplateInfo> createTestDataTemplate(uint8_t magic_number) {
+		boost::shared_ptr<IpfixRecord::DataTemplateInfo> testTemplate(new IpfixRecord::DataTemplateInfo);
+		testTemplate->templateId = magic_number + 256;
+		testTemplate->preceding = 0;
+		testTemplate->fieldCount = 1;
+		testTemplate->fieldInfo = (IpfixRecord::FieldInfo*)malloc(testTemplate->fieldCount * sizeof(IpfixRecord::FieldInfo));
+		testTemplate->dataCount = 1;
+		testTemplate->dataInfo = (IpfixRecord::FieldInfo*)malloc(testTemplate->fieldCount * sizeof(IpfixRecord::FieldInfo));
+		testTemplate->data = (uint8_t*)malloc(1); testTemplate->data[0] = magic_number;
+		testTemplate->fieldInfo[0].type.id = magic_number;
+		testTemplate->fieldInfo[0].type.length = 1;
+		testTemplate->fieldInfo[0].type.eid = 0;
+		testTemplate->fieldInfo[0].offset = 0;
+		testTemplate->dataInfo[0].type.id = magic_number;
+		testTemplate->dataInfo[0].type.length = 1;
+		testTemplate->dataInfo[0].type.eid = 0;
+		testTemplate->dataInfo[0].offset = 0;
+		testTemplate->userData = 0;
+
+		return testTemplate;
+}
+
 boost::shared_ptr<IpfixRecord::SourceID> createTestSourceId(uint8_t magic_number) {
 		boost::shared_ptr<IpfixRecord::SourceID> testSourceId(new IpfixRecord::SourceID);
-		testSourceId->observationDomainId = 0;
+		testSourceId->observationDomainId = 0xdeadbeef;
 		testSourceId->exporterAddress.ip[0] = magic_number;
-		testSourceId->exporterAddress.len = 1;
+		testSourceId->exporterAddress.ip[1] = magic_number;
+		testSourceId->exporterAddress.ip[2] = magic_number;
+		testSourceId->exporterAddress.ip[3] = magic_number;
+		testSourceId->exporterAddress.len = 4;
 
 		return testSourceId;
 }
 		
-boost::shared_array<uint8_t> createTestData(uint8_t magic_number) {
-		boost::shared_array<uint8_t> testData(new uint8_t[1]);
-		testData.get()[0] = magic_number;
-		return testData;
-}
-
-boost::shared_ptr<IpfixDataRecord> createTestRecord(uint8_t magic_number) {
+boost::shared_ptr<IpfixDataRecord> createTestDataRecord(uint8_t magic_number, boost::shared_ptr<IpfixRecord::SourceID> sourceId, boost::shared_ptr<IpfixRecord::TemplateInfo> templateInfo) {
 		boost::shared_ptr<IpfixDataRecord> testRecord(new IpfixDataRecord);
-		testRecord->sourceID = createTestSourceId(magic_number);
-		testRecord->templateInfo = createTestTemplate(magic_number);
+		testRecord->sourceID = sourceId;
+		testRecord->templateInfo = templateInfo;
 		testRecord->dataLength = 1;
 		testRecord->message = createTestData(magic_number);
 		testRecord->data = testRecord->message.get();
+
+		return testRecord;
+}
+
+boost::shared_ptr<IpfixTemplateDestructionRecord> createTestTemplateDestructionRecord(uint8_t magic_number, boost::shared_ptr<IpfixRecord::TemplateInfo> templateInfo) {
+		boost::shared_ptr<IpfixTemplateDestructionRecord> testRecord(new IpfixTemplateDestructionRecord);
+		testRecord->templateInfo = templateInfo;
+
+		return testRecord;
+}
+
+boost::shared_ptr<IpfixDataDataRecord> createTestDataDataRecord(uint8_t magic_number, boost::shared_ptr<IpfixRecord::SourceID> sourceId, boost::shared_ptr<IpfixRecord::DataTemplateInfo> dataTemplateInfo) {
+		boost::shared_ptr<IpfixDataDataRecord> testRecord(new IpfixDataDataRecord);
+		testRecord->sourceID = sourceId;
+		testRecord->dataTemplateInfo = dataTemplateInfo;
+		testRecord->dataLength = 1;
+		testRecord->message = createTestData(magic_number);
+		testRecord->data = testRecord->message.get();
+
+		return testRecord;
+}
+
+boost::shared_ptr<IpfixTemplateRecord> createTestTemplateRecord(uint8_t magic_number, boost::shared_ptr<IpfixRecord::TemplateInfo> templateInfo) {
+		boost::shared_ptr<IpfixTemplateRecord> testRecord(new IpfixTemplateRecord);
+		testRecord->templateInfo = templateInfo; 
+
+		return testRecord;
+}
+
+boost::shared_ptr<IpfixDataTemplateRecord> createTestDataTemplateRecord(uint8_t magic_number, boost::shared_ptr<IpfixRecord::DataTemplateInfo> dataTemplateInfo) {
+		boost::shared_ptr<IpfixDataTemplateRecord> testRecord(new IpfixDataTemplateRecord);
+		testRecord->dataTemplateInfo = dataTemplateInfo; 
+
+		return testRecord;
+}
+
+boost::shared_ptr<IpfixDataTemplateDestructionRecord> createTestDataTemplateDestructionRecord(uint8_t magic_number, boost::shared_ptr<IpfixRecord::DataTemplateInfo> dataTemplateInfo) {
+		boost::shared_ptr<IpfixDataTemplateDestructionRecord> testRecord(new IpfixDataTemplateDestructionRecord);
+		testRecord->dataTemplateInfo = dataTemplateInfo;
 
 		return testRecord;
 }
@@ -83,13 +187,17 @@ void test_module_coupling() {
 	std::cout << "Testing: Concentrator module coupling..." << std::endl;
 
 	// create testSink
-	TestSink testSink;
+	TestSink testSink(true);
 	testSink.runSink();
 
 	// create some test data
-	std::vector<boost::shared_ptr<IpfixRecord> > testRecords;
+	std::vector<boost::shared_ptr<IpfixDataRecord> > testRecords;
 	for (uint8_t magic_number = 0; magic_number < 255; magic_number++) {
-		testRecords.push_back(createTestRecord(magic_number));	
+		boost::shared_ptr<IpfixRecord::SourceID> testSourceId = createTestSourceId(magic_number);
+		boost::shared_ptr<IpfixRecord::TemplateInfo> templateInfo = createTestTemplate(magic_number);
+		boost::shared_ptr<IpfixTemplateRecord> dtr = createTestTemplateRecord(magic_number, templateInfo);
+		testSink.push(dtr);
+		testRecords.push_back(createTestDataRecord(magic_number, testSourceId, templateInfo));	
 	}
 
 	// perform test
@@ -103,45 +211,135 @@ void test_module_coupling() {
 	for (int i = 0; i < 128; i++) {
 		while (testSink.getQueueLength() > 5);
 		int magic_number = rand() % 3;
-		if ((i % 10) == 0) testRecords[magic_number] = createTestRecord(magic_number);
+		if ((i % 10) == 0) testRecords[magic_number] = createTestDataRecord(magic_number, testRecords[magic_number]->sourceID, testRecords[magic_number]->templateInfo);
 		testSink.push(testRecords[magic_number]);
 	}
 
 
 	// finish test
 	sleep(1);
-	TimeoutSemaphore::shutdown();
+
+	//TimeoutSemaphore::shutdown();
+	// make sure that pop semaphore doesn't hang. This shouldn't be needed, but let's play it safe
+	for (uint8_t magic_number = 0; magic_number < 255; magic_number++) {
+		testSink.push(createTestTemplateDestructionRecord(magic_number, testRecords[magic_number]->templateInfo));
+	}
 	testSink.terminateSink();
 	sleep(1);
-	testSink.push(testRecords[0]);
-
-
 
 }
 
+void test_ipfixlolib_rawdir() {
+#ifndef IPFIXLOLIB_RAWDIR_SUPPORT
+	std::cout << "Not Testing: Ipfixlolib Rawdir reading/writing" << std::endl;
+#endif
+#ifdef IPFIXLOLIB_RAWDIR_SUPPORT
+
+	std::cout << "Testing: Ipfixlolib Rawdir writing..." << std::endl;
+
+	{
+		// create IpfixRawdirWriter
+		IpfixRawdirWriter ipfixRawdirWriter(0xbeef, "/tmp/pack2");
+		ipfixRawdirWriter.runSink();
+
+		// create some test data
+		std::vector<boost::shared_ptr<IpfixDataDataRecord> > testDataRecords;
+		for (uint8_t magic_number = 0; magic_number < 16; magic_number++) {
+			boost::shared_ptr<IpfixRecord::SourceID> testSourceId = createTestSourceId(magic_number);
+			boost::shared_ptr<IpfixRecord::DataTemplateInfo> dataTemplateInfo = createTestDataTemplate(magic_number);
+			boost::shared_ptr<IpfixDataTemplateRecord> dtr = createTestDataTemplateRecord(magic_number, dataTemplateInfo);
+			ipfixRawdirWriter.push(dtr);
+			ipfixRawdirWriter.onIdle();
+			testDataRecords.push_back(createTestDataDataRecord(magic_number, testSourceId, dataTemplateInfo));
+		}
+
+		// perform test	
+		for (uint8_t magic_number = 0; magic_number < 16; magic_number++) {
+			ipfixRawdirWriter.push(testDataRecords[magic_number]);
+			ipfixRawdirWriter.onIdle();
+		}
+
+		// finish test
+		sleep(1);
+
+		//TimeoutSemaphore::shutdown();
+		// make sure that pop semaphore doesn't hang. This shouldn't be needed, but let's play it safe
+		for (uint8_t magic_number = 0; magic_number < 16; magic_number++) {
+			ipfixRawdirWriter.push(createTestDataTemplateDestructionRecord(magic_number, testDataRecords[magic_number]->dataTemplateInfo));
+		}
+		ipfixRawdirWriter.terminateSink();
+		sleep(1);
+
+	}
+
+	std::cout << "Testing: Ipfixlolib Rawdir reading..." << std::endl;
+	{
+		// create testSink
+		TestSink testSink(false);
+		testSink.runSink();
+
+		IpfixRawdirReader ipfixRawdirReader("/tmp/pack2");
+
+		IpfixParser ipfixParser;
+		ipfixParser.addFlowSink(&testSink);
+
+		IpfixCollector ipfixCollector;
+		ipfixCollector.addIpfixReceiver(&ipfixRawdirReader);
+		ipfixCollector.addIpfixPacketProcessor(&ipfixParser);
+		ipfixCollector.start();
+
+		// finish test
+		sleep(1);
+
+		//TimeoutSemaphore::shutdown();
+		// make sure that pop semaphore doesn't hang. This shouldn't be needed, but let's play it safe
+		testSink.push(createTestDataTemplateDestructionRecord(0, createTestDataTemplate(0)));
+		testSink.terminateSink();
+		sleep(1);
+
+		if (testSink.receivedRecords != 16) {
+			char s[256];
+			snprintf(s, 255, "IpfixRawdirReader should have read 16 records, but read %d", testSink.receivedRecords);
+			BOOST_ERROR(s);
+		}
+	}
+
+#endif
+}
+
+void test_parser_stability() {
+
+	boost::shared_ptr<IpfixRecord::SourceID> testSourceId = createTestSourceId(42);
+
+	IpfixParser ipfixParser;
+
+	for (int iteration = 0; iteration < 65000; iteration++) {
+		for (int len = 32; len < 64; len++) {
+			boost::shared_array<uint8_t> message(new uint8_t[len]);
+			for (int i = 0; i < len; i++) message[i]=rand();
+			*((uint16_t*)message.get())=htons(0x000a);
+			*((uint16_t*)(message.get()+2))=htons(len);
+
+			ipfixParser.processPacket(message, len, testSourceId); 
+		}
+	}
+}
+
 int test_main(int, char *[]) {
-	
+
 	// set Vermont messaging subsystem's debug level
 	//msg_setlevel(MSG_DEFAULT+99);
 
 	// seed random number generator to 0
 	srand(0);	
-	
+
 	std::cout << "Testing: Concentrator" << std::endl;
-	test_module_coupling();
 
-	/*
-	// six ways to detect and report the same error:
-	BOOST_CHECK( add( 2,2 ) == 4 );        // #1 continues on error
-	BOOST_REQUIRE( add( 2,2 ) == 4 );      // #2 throws on error
-	if( add( 2,2 ) != 4 )
-	BOOST_ERROR( "Ouch..." );            // #3 continues on error
-	if( add( 2,2 ) != 4 )
-	BOOST_FAIL( "Ouch..." );             // #4 throws on error
-	if( add( 2,2 ) != 4 ) throw "Oops..."; // #5 throws on error
+	//test_module_coupling();
 
-	return add( 2, 2 ) == 4 ? 0 : 1;       // #6 returns error code
-	*/
+	test_ipfixlolib_rawdir();
+
+	//test_parser_stability();
 
 	return 0;
 }
