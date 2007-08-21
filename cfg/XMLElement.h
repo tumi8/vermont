9dffb8cae1ceb0ec20df19be14c2c3861ede92ad
siprbaum <siprbaum@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-08-21 12:17:23 +0000

    Implement methods to get the attribute(s) of an XMLElement
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont-dynamic-config@1059 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- cfg/XMLElement.h
+++ cfg/XMLElement.h
@@ -5,15 +5,26 @@
 #include <libxml/tree.h>
 
 #include "cfg/XMLNode.h"
+#include "cfg/XMLAttribute.h"
 
 #include <string>
 #include <vector>
 
+class XMLAttribute;
+
 class XMLElement: public XMLNode
 {
 public:
+	typedef std::vector<XMLAttribute*> XMLAttributeSet;
+
 	XMLElement(xmlNodePtr ptr);
 	virtual ~XMLElement();
+
+	XMLAttributeSet getAttributes();
+	XMLAttribute* getAttribute(const std::string& name);
+
+private:
+	XMLAttributeSet getAttribHelper(const std::string&);
 };
 
 #endif /*XMLNODE_H_*/
