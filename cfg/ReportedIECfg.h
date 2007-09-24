a745fba349f543854adcd507657c2c58a4e1660e
siprbaum <siprbaum@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2007-09-24 15:00:46 +0000

    Refactor ReportedIE to use the xml framework
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont-dynamic-config@1258 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- cfg/ReportedIECfg.h
+++ cfg/ReportedIECfg.h
@@ -1,42 +1,55 @@
 #ifndef REPORTEDIECFG_H_
 #define REPORTEDIECFG_H_
 
-#include <ipfix_configuration.h>
+#include "Cfg.h"
 #include <string>
 
-class Configuration::ReportedIE {
+class ReportedIECfg
+	: public Cfg {
 public:
-	ReportedIE(xmlNodePtr p, const Configuration& m)
-		: ieLength(-1), ieId(-1)
+	ReportedIECfg(XMLElement* _elem)
+		: Cfg(_elem), ieLength(-1), ieId(-1)
 
 	{
-		/*
-		xmlNodePtr i = p->xmlChildrenNode;
-		while (NULL != i) {
-			if (m.tagMatches(i, "ieId")) {
-				ieId = atoi(m.getContent(i).c_str());
-				ieName = m.getContent(i);
-			} else if (m.tagMatches(i, "ieLength")) {
-				ieLength = atoi(m.getContent(i).c_str());
-			} else if (m.tagMatches(i, "ieName")) {
-				ieName = m.getContent(i);
-			}
-			i = i->next;
+		if (!_elem)
+			return;
+		
+		ieLength = getInt("ieLength", -1);
+		ieId     = getInt("ieId", -1);
+	
+		try {
+			ieName = get("ieName");
+			std::transform(ieName.begin(), ieName.end(), ieName.begin(), std::tolower);
+		} catch(IllegalEntry ie) {
+			THROWEXCEPTION("Missing 'ieName' in the config");
 		}
-		std::transform(ieName.begin(), ieName.end(), ieName.begin(), std::tolower);
-		*/
+	}
+	
+	std::string getName() const { return "reportedIE"; }
+	
+	virtual bool deriveFrom(Cfg* old)	{
+		ReportedIECfg* cfg = dynamic_cast<ReportedIECfg*>(old);
+		if (cfg)
+			return deriveFrom(cfg);
+
+		THROWEXCEPTION("Can't derive from PacketCountFilter");
+		return false;
 	}
 
+	virtual bool deriveFrom(ReportedIECfg* old)
+	{
+		return true;
+	}
+	
 	bool hasOptionalLength() const { return ieLength != -1; }
-	std::string getName() const { return ieName; }
+
+	std::string get_ieName() const { return ieName; }
+	
 	unsigned getLength() const { return ieLength; }
-	unsigned getId() const { return (ieId == -1)?ipfix_name_lookup(ieName.c_str()):ieId; }
+
+//	unsigned get_ieId() const { return (ieId == -1) ? ipfix_name_lookup(ieName.c_str()) : ieId; }
 
 private:
-	std::string getContent(xmlDocPtr doc, xmlNodePtr p);
-
-	bool xmlCompare(const xmlNodePtr node, const std::string& tagName);
-
 	std::string ieName;
 	int ieLength;
 	int ieId;
