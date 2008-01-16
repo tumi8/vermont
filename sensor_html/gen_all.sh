491ad970d4bce5038121f43085da611540b76669
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-01-16 17:03:01 +0000

    changed location of expected sensor_output.xml to current directory
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont-dynamic-config@1526 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- sensor_html/gen_all.sh
+++ sensor_html/gen_all.sh
@@ -3,8 +3,8 @@
 while `true`; do
 	(
 		flock -s 200
-		xsltproc sensor_output.xsl ../sensor_output.xml > sensor_output.html
-		cat ../sensor_output.xml | perl gen_dot.pl | dot -Tpng -o modules.png
+		xsltproc sensor_output.xsl sensor_output.xml > sensor_output.html
+		cat sensor_output.xml | perl gen_dot.pl | dot -Tpng -o modules.png
 	) 200> ../sensor_output.lock
 	sleep 1
 done
