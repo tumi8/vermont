32102d8f588557480946cada7f246dac0bb7a1d4
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-06-12 17:04:50 +0000

    comments changed (from r1551)
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont-dynamic-config@1707 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- concentrator/BaseHashtable.cpp
+++ concentrator/BaseHashtable.cpp
@@ -91,12 +91,12 @@ void BaseHashtable::createDataTemplate(Rule* rule)
 			dataTemplate->data = (IpfixRecord::Data*)realloc(dataTemplate->data, dataLength);
 			memcpy(dataTemplate->data + fi->offset, rf->pattern, fi->type.length);
 		}
-		/* gerhard: If we have a pattern (and fixed value field), the variable length field is implicitely discarded.
+		/* gerhard: If we have a pattern (and fixed value field), the variable value field is implicitely discarded.
 		 * Note: This is necessary because of the double meaning/usage of Rule::Field.type.length: 
 		 * If a pattern is present, this variable holds the length of the pattern (or fixed length field) 
 		 * and not the length of the normal field holding a single value. As a consequence, we cannot use
-		 * Rule.Field.type.length as length of the variable field.
-		 * If someone really wants to enable the export of both, pattern and variable field of the same
+		 * Rule.Field.type.length as length of the variable value field.
+		 * If someone really wants to enable the export of both, pattern and variable value field of the same
 		 * type, then he has to remove the double meaning/usage (or set the variable field length to the
 		 * default value for the specific type). 
 		 */
