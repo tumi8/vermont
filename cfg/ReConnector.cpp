98e8c2a5097e16dcd91323eb876b2847eb72b3c1
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2008-08-28 13:22:10 +0000

    made output during reconfiguration nicer
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont-dynamic-config@1820 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- cfg/ReConnector.cpp
+++ cfg/ReConnector.cpp
@@ -15,18 +15,18 @@ Graph* ReConnector::connect(Graph* g)
 		topoOld[i]->getCfg()->getInstance()->preReconfiguration();
 		topoOld[i]->getCfg()->disconnectInstances();
 	}
-	
+
 	/* call onReconfiguration1 on all modules */
 	for (size_t i = 0; i < topoOld.size(); i++) {
 		topoOld[i]->getCfg()->onReconfiguration1();
 	}
-	
+
 	/* call preConfiguration2 on all modules */
 	for (size_t i = 0; i < topoOld.size(); i++) {
 		topoOld[i]->getCfg()->onReconfiguration2();
 	}
-	
-	
+
+
 	// compare the nodes in the old and new graph and search for
 	// (nearly) identical modules which could be reused
 	for (size_t i = 0; i < topoOld.size(); i++) {
@@ -34,17 +34,17 @@ Graph* ReConnector::connect(Graph* g)
 		for (size_t j = 0; j < topoNew.size(); j++) {
 			Cfg* newCfg = topoNew[j]->getCfg();
 			if (oldCfg->getID() == newCfg->getID()) { // possible match
-				msg(MSG_INFO, "found a match between %s(id=%d) -> %s(id=%d)\n",
+				msg(MSG_INFO, "found a match between %s(id=%d) -> %s(id=%d)",
 						oldCfg->getName().c_str(), oldCfg->getID(),
 						newCfg->getName().c_str(), newCfg->getID());
 
 				// check if we could use the same module instance in the new config
 				if (newCfg->deriveFrom(oldCfg)) {
-					msg(MSG_INFO, "reusing %s(id=%d)\n",
+					msg(MSG_INFO, "reusing %s(id=%d)",
 							oldCfg->getName().c_str(), oldCfg->getID());
 					newCfg->transferInstance(oldCfg);
 				} else {
-					msg(MSG_INFO, "can't reuse %s(id=%d)\n",
+					msg(MSG_INFO, "can't reuse %s(id=%d)",
 							oldCfg->getName().c_str(), oldCfg->getID());
 				}
 			}
