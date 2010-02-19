f737cf79f94a9e5690cd002eb947eabdee13c635
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2010-02-19 13:41:23 +0000

    added IpfixNetflowExporter (allows Netflow.v5 exports)
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/trunk/vermont@2264 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- src/common/defs.h
+++ src/common/defs.h
@@ -88,6 +88,17 @@
  */
 #define HT_DEFAULT_BITSIZE 17
 
+/**
+ * defines in milliseconds, how long flow records may be cached in IpfixNetflowExporter
+ * until they are sent to the network
+ */
+#define INE_DEFAULT_RECORDCACHETIMEOUT 5000
+
+/**
+ * defines in packets per seconds, how many udp records are to be sent over
+ * the wire by IpfixNetflowExporter
+ */
+#define INE_DEFAULT_MAXRECORDRATE 5000
 
 
 /**
