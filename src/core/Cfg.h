91a75a67450d8be8f9561b8f3d186b019ad3686c
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2010-06-16 14:07:22 +0000

    bugfix in module connection: if two modules were connected to a "Notifiable" module without ConnectionQueue in between, a ConnectionQueue would have been created once, but connected to the same module twice
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont/lockfree@2427 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- src/core/Cfg.h
+++ src/core/Cfg.h
@@ -261,12 +261,11 @@ public:
 		if (!queue) {
 			msg(MSG_DIALOG, "queue is required by module id=%u but is not configured. Inserting a default queue with max size 1 (attention: this is inefficient!)", getID());
 			queue = new ConnectionQueue<typename InstanceType::dst_value_type>(1);
+			queue->connectTo(getInstance());
+			Notifiable* n = dynamic_cast<Notifiable*>(getInstance());
+			if (n) n->useTimer(queue);
 		}
 
-		queue->connectTo(getInstance());
-		Notifiable* n = dynamic_cast<Notifiable*>(instance);
-		if (n)
-			n->useTimer(queue);
 		return queue;
 	}
 
