68c3d7c47c1c253d17254355f7c634b5873fa8f4
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2010-07-14 13:45:48 +0000

    PCAPExporterPipe: made output a bit less verbose, added configuration variable 'workingpath', cleaned up code
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont/dtls-merge@2477 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- src/modules/packet/PCAPExporterPipe.cpp
+++ src/modules/packet/PCAPExporterPipe.cpp
@@ -7,12 +7,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -40,18 +40,32 @@
 namespace bfs = boost::filesystem;
 
 PCAPExporterPipe::PCAPExporterPipe(const std::string& logfile)
-	: logFileName(logfile), fifoReaderCmd(""), onRestart(false),
-	appenddate(false),  restartOnSignal(false),fifoReaderPid(0), 
-	dummy(NULL), sigKillTimeout(1),counter(0), last_check(0)
+	: logFileName(logfile),
+	  fifoReaderCmd(""),
+	  workingPath(""),
+	  onRestart(false),
+	  appenddate(false),
+	  restartOnSignal(false),
+	  fifoReaderPid(0),
+	  dummy(NULL),
+	  sigKillTimeout(1),
+	  counter(0),
+	  last_check(0)
 {
 }
 
 PCAPExporterPipe::~PCAPExporterPipe()
 {
 }
+
 void PCAPExporterPipe::setPipeReaderCmd(const std::string& cmd)
 {
 	fifoReaderCmd = cmd;
+}
+
+void PCAPExporterPipe::setWorkingPath(const std::string& path)
+{
+	workingPath = path;
 }
 
 void PCAPExporterPipe::setAppendDate(bool value)
@@ -108,10 +122,17 @@ int PCAPExporterPipe::execCmd(std::string& cmd)
 
 	int pid = fork();
 	if (pid == 0) {
-		if (dup2(fd[0], STDIN_FILENO) == -1) { 
-			if (write(child_parent_pipe[1], &errno, sizeof(int)) != sizeof(int)) 
+		if (dup2(fd[0], STDIN_FILENO) == -1) {
+			if (write(child_parent_pipe[1], &errno, sizeof(int)) != sizeof(int))
 				THROWEXCEPTION("dup(fd[0]) failed");
 				exit(1);
+		}
+		if (workingPath != "") {
+			int res = chdir(workingPath.c_str());
+			if (res != 0) {
+				THROWEXCEPTION("failed to change to working path '%s'", workingPath.c_str());
+				exit(1);
+			}
 		}
 		if (logFileName != "") {
 			std::string logfile = logFileName;
@@ -122,7 +143,7 @@ int PCAPExporterPipe::execCmd(std::string& cmd)
 				time (&rawtime);
 				timeinfo = localtime (&rawtime);
 				if (strftime(buffer, 20, "%Y-%m-%d_%H:%M:%S", timeinfo) == 0) {
-					if (write(child_parent_pipe[1], &errno, sizeof(int)) != sizeof(int)) 
+					if (write(child_parent_pipe[1], &errno, sizeof(int)) != sizeof(int))
 						THROWEXCEPTION("strftime & write failed");
 					exit(1);
 				}
@@ -162,7 +183,7 @@ int PCAPExporterPipe::execCmd(std::string& cmd)
 			THROWEXCEPTION("An error occurred in the child: %s", strerror(buf));
 		}
 		return pid;
-	} else { 
+	} else {
 		THROWEXCEPTION("fork() failed");
 	}
 	return -1;
@@ -249,21 +270,24 @@ void PCAPExporterPipe::startProcess()
 						fifoReaderCmd.c_str(), fifoReaderPid);
 }
 
-void PCAPExporterPipe::receive(Packet* packet) {
-	if(onRestart){
-		 msg(MSG_ERROR, "Can't process packets while restarting fifoReaderCmd");
+void PCAPExporterPipe::receive(Packet* packet)
+{
+	if (onRestart){
+		 DPRINTF("Dropping incoming packet, as attached process is not ready");
 		 return;
 	}
 	if(fifoReaderPid == 0){
 		 msg(MSG_VDEBUG, "fifoReaderPid = 0...this might happen during reconfiguration");
-		 return; 
+		 return;
 	}
 	writePCAP(packet);
 }
-void PCAPExporterPipe::handleSigPipe(int sig){
+void PCAPExporterPipe::handleSigPipe(int sig)
+{
 	handleSigChld(SIGCHLD);
 }
-void PCAPExporterPipe::handleSigChld(int sig){
+void PCAPExporterPipe::handleSigChld(int sig)
+{
 	if(onRestart || exitFlag || isRunning(fifoReaderPid)) return;
 	onRestart = true;
 	counter++;
@@ -271,15 +295,15 @@ void PCAPExporterPipe::handleSigChld(int sig){
 	time(&tmp);
 	if(tmp== (time_t) -1)
 		THROWEXCEPTION("time() failed");
-	
+
 	// reset the counter if the last restart was more than 5 seconds ago
-	if((tmp -last_check) > 5) counter = 0; 
+	if((tmp -last_check) > 5) counter = 0;
 
 	if(counter > 5 && (tmp - last_check ) < 5)
 		THROWEXCEPTION("Too many restarts in a short time period. Maybe your commandline is erroneous");
-		
+
 	if(!isRunning(fifoReaderPid)){
-		//waitpid(fifoReaderPid, NULL, 0); 
+		//waitpid(fifoReaderPid, NULL, 0);
 		msg(MSG_ERROR, "Process of fifoReaderCmd \'%s\' with fifoReaderPid %d is not running!",
 				fifoReaderCmd.c_str(), fifoReaderPid);
 		startProcess();
@@ -291,13 +315,15 @@ void PCAPExporterPipe::handleSigChld(int sig){
 	onRestart = false;
 }
 
-void PCAPExporterPipe::handleSigUsr2(int sig){
+void PCAPExporterPipe::handleSigUsr2(int sig)
+{
 	if (! restartOnSignal) return;
 	int pid = fifoReaderPid;
 	fifoReaderPid = 0;
 	kill_all(pid);
 	handleSigChld(sig);
 }
+
 void PCAPExporterPipe::kill_pid(int pid)
 {
 	int i = sigKillTimeout;
@@ -309,7 +335,7 @@ void PCAPExporterPipe::kill_pid(int pid)
 		sleep(1);
 	}
 	kill(pid, SIGKILL);
-	waitpid(fifoReaderPid, NULL, 0); 
+	waitpid(fifoReaderPid, NULL, 0);
 }
 
 bool PCAPExporterPipe::isRunning(int pid)
@@ -320,10 +346,11 @@ bool PCAPExporterPipe::isRunning(int pid)
 	return false;
 }
 
-bool PCAPExporterPipe::checkint(const char *my_string) {
+bool PCAPExporterPipe::checkint(const char *my_string)
+{
 	size_t stringlength = strlen(my_string);
 	for (size_t j=0; j<stringlength; j++)
-		if((int)my_string[j] < 0x30 || (int)my_string[j] > 0x39) 
+		if((int)my_string[j] < 0x30 || (int)my_string[j] > 0x39)
 			return false;
 
 	return true;
@@ -344,7 +371,7 @@ void PCAPExporterPipe::kill_all(int ppid)
 	bfs::directory_iterator dir_iterator = bfs::directory_iterator(path);
 	bfs::directory_iterator end_itr;
 	while (dir_iterator != end_itr) {
-		if (bfs::is_directory(dir_iterator->status())  && 
+		if (bfs::is_directory(dir_iterator->status())  &&
 				this->checkint(dir_iterator->leaf().c_str()))
 		{
 			std::string filename = dir_iterator->path().file_string() + "/stat";
