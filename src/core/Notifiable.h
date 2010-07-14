5b3a8fe2b8e597d00d435f38b55f6598e37472ce
limmer <limmer@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2010-07-14 13:45:30 +0000

    bugfix: in virtual class Notifiable timer was not set to zero during initialization
    git-svn-id: https://svn.berlios.de/svnroot/repos/vermont/branches/vermont/dtls-merge@2469 aef3b71b-58ee-0310-9ba9-8811b9f0742f

--- src/core/Notifiable.h
+++ src/core/Notifiable.h
@@ -6,12 +6,12 @@
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -31,17 +31,18 @@ class Timer;
 class Notifiable
 {
 public:
+	Notifiable() : timer(0) {}
 	virtual ~Notifiable() {}
-	
+
 	/**
 	 * function is called after the specified timeout in function
 	 * Timer::addTimeout
 	 * @param flag flag which was passed to Timer::addTimeout
 	 */
 	virtual void onTimeout(void* dataPtr) = 0;
-	
+
 	inline void useTimer(Timer* t) { timer = t; }
-	
+
 protected:
 	Timer* timer;
 };
