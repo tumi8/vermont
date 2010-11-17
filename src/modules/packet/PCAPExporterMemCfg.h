03df66fbd37a05697f8811879af4d6b603084e20
sithhaue <sithhaue@aef3b71b-58ee-0310-9ba9-8811b9f0742f>
2010-11-17 10:25:27 +0000

    added module PCAPExporterMem for communication between snort and vermont via shared memory
    git-svn-id: file:///Users/braun/svn/vermont/branches/vermont/loadbalancer@2584 aef3b71b-58ee-0310-9ba9-8811b9f0742f

---
+++ src/modules/packet/PCAPExporterMemCfg.h
@@ -0,0 +1,60 @@
+/*
+ * Vermont Configuration Subsystem
+ * Copyright (C) 2009 Vermont Project
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#ifndef PCAPEXPORTERMEMCFG_H_
+#define PCAPEXPORTERMEMCFG_H_
+
+#include "core/Cfg.h"
+#include "modules/packet/PCAPExporterMem.h"
+#include "modules/packet/PCAPExporterBase.h"
+
+#include <vector>
+
+
+class PCAPExporterMemCfg
+	: public CfgHelper<PCAPExporterMem, PCAPExporterMemCfg>
+{
+	friend class ConfigManager;
+public:
+	virtual ~PCAPExporterMemCfg();
+
+	virtual PCAPExporterMemCfg* create(XMLElement* elem);
+
+	virtual PCAPExporterMem* createInstance();
+
+	bool deriveFrom(PCAPExporterMemCfg* old);
+
+protected:
+	PCAPExporterMemCfg(XMLElement* elem);
+
+private:
+	int link_type;
+	int snaplen;
+    int sigkilltimeout;
+	std::string logFileName;
+    std::string fifoReaderCmd;
+    std::string workingPath;
+	bool appenddate;
+	bool restart;
+	uint32_t restartInterval;
+};
+
+
+#endif /*PCAP_EXPORTERFILECFG_H_*/
