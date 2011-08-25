0a010e7330e989ab3725d31d2823d508fec261ad
Philipp Fehre <philipp.fehre@googlemail.com>
2011-08-25 07:17:19 -0700

    Added Cmake options for oracle

---
+++ cmake/modules/FindOracle.cmake
@@ -0,0 +1,40 @@
+# - Find Oracle
+# Find the Oracle includes and client library
+# This module defines
+#  ORACLE_INCLUDE_DIR, where to find occi.h
+#  ORACLE_LIBRARIES, the libraries needed to use ORACLE.
+#  ORACLE_FOUND, If false, do not try to use Oracle.
+#
+# Copyright (c) 2011, Philipp Fehre, <philipp.fehre@googlemail.com>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+# Add the oracle include paths here
+
+if(ORACLE_INCLUDE_DIR AND ORACLE_LIBRARIES)
+   set(ORACLE_FOUND TRUE)
+
+else(ORACLE_INCLUDE_DIR AND ORACLE_LIBRARIES)
+
+	find_path(ORACLE_INCLUDE_DIR occi.h
+	  /usr/lib/oracle/xe/app/oracle/product/10.2.0/client/rdbms/public
+    )
+
+  find_library(ORACLE_LIBRARIES NAMES libocci libclntsh
+    PATHS
+    /usr/lib/oracle/xe/app/oracle/product/10.2.0/client/lib
+    )
+      
+  if(ORACLE_INCLUDE_DIR AND ORACLE_LIBRARIES)
+    set(ORACLE_FOUND TRUE)
+    message(STATUS "Found Oracle: ${ORACLE_INCLUDE_DIR}, ${ORACLE_LIBRARIES}")
+    INCLUDE_DIRECTORIES(${ORACLE_INCLUDE_DIR})
+  else(ORACLE_INCLUDE_DIR AND ORACLE_LIBRARIES)
+    set(ORACLE_FOUND FALSE)
+    message(STATUS "Oracle not found.")
+  endif(ORACLE_INCLUDE_DIR AND ORACLE_LIBRARIES)
+
+  mark_as_advanced(ORACLE_INCLUDE_DIR ORACLE_LIBRARIES)
+
+endif(ORACLE_INCLUDE_DIR AND ORACLE_LIBRARIES)
--- src/modules/ipfix/IpfixDbWriterOracle.hpp
+++ src/modules/ipfix/IpfixDbWriterOracle.hpp
@@ -17,3 +17,119 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
+
+/* Some constants that are common to IpfixDbWriter and IpfixDbReader */
+#ifdef ORACLE_SUPPORT_ENABLED
+
+#ifndef IPFIXDBWRITERORACLE_H
+#define IPFIXDBWRITERORACLE_H
+
+#include "IpfixDbCommon.hpp"
+#include "IpfixRecordDestination.h"
+#include "common/ipfixlolib/ipfix.h"
+#include "common/ipfixlolib/ipfixlolib.h"
+#include <occi.h>
+#include <iostream>
+#include <iomanip>
+#include <stdlib.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <time.h>
+#include <sstream>
+
+using namespace oracle::occi;
+using namespace std;
+
+#define EXPORTERID 0
+
+/**
+ * IpfixDbWriterOracle powered the communication to the oracle database server
+ * also between the other structs
+ */
+class IpfixDbWriterOracle 
+	: public IpfixRecordDestination, public Module, public Source<NullEmitable*>
+{
+	public:
+		IpfixDbWriterOracle(const string& hostname, const string& dbname,
+				const string& username, const string& password,
+				unsigned port, uint32_t observationDomainId, unsigned maxStatements,
+				const vector<string>& columns);
+		~IpfixDbWriterOracle();
+
+		void onDataRecord(IpfixDataRecord* record);
+
+		/**
+		 * Struct to identify the relationship between columns names and 
+		 * IPFIX_TYPEID, column type and default value
+		 */
+		struct Column {
+			const char* columnName; 	/** column name */
+			const char* columnType;		/** column data type in database */
+			uint64_t defaultValue;       	/** default value */
+			InformationElement::IeId ipfixId; /** IPFIX_TYPEID */
+			InformationElement::IeEnterpriseNumber enterprise; /** enterprise number */
+		};
+
+	private:
+		static const unsigned MAX_EXPORTER = 10;    // maximum numbers of cached exporters
+
+		/**
+		 * Struct buffers start and end time and tablename for the different tables
+		 */
+		struct TableCacheEntry {
+			time_t startTime; // smallest flow start second timestamp in the table
+			time_t endTime;   // largest flow start second timestamp in the table
+			string name;   // name of the table
+		};
+
+		/**
+		 * Struct buffers ODID, IP address and row index of an exporter
+		 */
+		struct ExporterCacheEntry {
+			IpfixRecord::SourceID sourceID;/** source id of the exporter */
+			int id;                        /** Id entry of sourcID and expIP in the ExporterTable */
+		};
+
+
+		TableCacheEntry currentTable;				// current table in tableCache
+
+		list<ExporterCacheEntry> exporterCache;		// cached tables names, key=observationDomainId
+		ExporterCacheEntry* currentExporter;			// pointer to current exporter in exporterCache
+
+		IpfixRecord::SourceID srcId;           			// default source ID
+
+		ostringstream insertStatement;			// insert statement string
+		int numberOfInserts;					// number of inserts in statement
+		int maxInserts;						// maximum number of inserts per statement
+
+		vector<Column> tableColumns;			// table columns
+		string tableColumnsString;     			// table columns as string for INSERT statements
+		string tableColumnsCreateString;  			// table columns as string for CREATE statements
+
+		// database data
+		string dbHost, dbName, dbUser, dbPassword;
+		unsigned dbPort;
+		Environment *env;
+		Connection *con;
+
+		int createDB();
+		int setCurrentTable(time_t flowstartsec);
+		string& getInsertString(string& row, time_t& flowstartsec, const IpfixRecord::SourceID& sourceID,
+				TemplateInfo& dataTemplateInfo,uint16_t length, IpfixRecord::Data* data);
+		int writeToDb();
+		int getExporterID(const IpfixRecord::SourceID& sourceID);
+		int connectToDB();
+		void processDataDataRecord(const IpfixRecord::SourceID& sourceID, 
+				TemplateInfo& dataTemplateInfo, uint16_t length, 
+				IpfixRecord::Data* data);
+
+		uint64_t getData(InformationElement::IeInfo type, IpfixRecord::Data* data);
+		bool equalExporter(const IpfixRecord::SourceID& a, const IpfixRecord::SourceID& b);
+};
+
+
+#endif
+
+
+#endif
+
