337c1f669ad8556121ae5a5e93f798d21f678b49
Philipp Fehre <philipp.fehre@googlemail.com>
2011-09-01 07:10:49 -0700

    Added configurator

--- src/modules/ipfix/IpfixDbWriterOracleCfg.cpp
+++ src/modules/ipfix/IpfixDbWriterOracleCfg.cpp
@@ -1,5 +1,5 @@
 /*
- * IPFIX Database Reader/Writer Oracle Connector
+ * IPFIX Database Reader/Writer Oracle Connector Configuration
  * Copyright (C) 2011 Philipp Fehre <philipp.fehre@googlemail.com>
  *
  * This program is free software; you can redistribute it and/or
@@ -17,3 +17,93 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
+
+#ifdef ORACLE_SUPPORT_ENABLED
+
+#include "IpfixDbWriterOracleCfg.h"
+
+
+IpfixDbWriterOracleCfg* IpfixDbWriterOracleCfg::create(XMLElement* e)
+{
+    assert(e);
+    assert(e->getName() == getName());
+    return new IpfixDbWriterOracleCfg(e);
+}
+
+
+IpfixDbWriterOracleCfg::IpfixDbWriterOracleCfg(XMLElement* elem)
+    : CfgHelper<IpfixDbWriterOracle, IpfixDbWriterOracleCfg>(elem, "ipfixDbWriter"),
+      port(0), bufferRecords(30), observationDomainId(0)
+{
+    if (!elem) return;
+
+    XMLNode::XMLSet<XMLElement*> set = _elem->getElementChildren();
+	for (XMLNode::XMLSet<XMLElement*>::iterator it = set.begin();
+	     it != set.end();
+	     it++) {
+		XMLElement* e = *it;
+
+		if (e->matches("host")) {
+			hostname = e->getFirstText();
+		} else if (e->matches("port")) {
+			port = getInt("port");
+		} else if (e->matches("dbname")) {
+			dbname = e->getFirstText();
+		} else if (e->matches("username")) {
+			user = e->getFirstText();
+		} else if (e->matches("password")) {
+			password = e->getFirstText();
+		} else if (e->matches("bufferrecords")) {
+			bufferRecords = getInt("bufferrecords");
+		} else if (e->matches("columns")) {
+			readColumns(e);
+		} else if (e->matches("observationDomainId")) {
+			observationDomainId = getInt("observationDomainId");
+		} else if (e->matches("next")) { // ignore next
+		} else {
+			msg(MSG_FATAL, "Unknown IpfixDbWriterOracle config statement %s\n", e->getName().c_str());
+			continue;
+		}
+	}
+	if (hostname=="") THROWEXCEPTION("IpfixDbWriterOracleCfg: host not set in configuration!");
+	if (port==0) THROWEXCEPTION("IpfixDbWriterOracleCfg: port not set in configuration!");
+	if (dbname=="") THROWEXCEPTION("IpfixDbWriterOracleCfg: dbname not set in configuration!");
+	if (user=="") THROWEXCEPTION("IpfixDbWriterOracleCfg: username not set in configuration!");
+}
+
+void IpfixDbWriterOracleCfg::readColumns(XMLElement* elem) {
+	colNames.clear();
+	XMLNode::XMLSet<XMLElement*> set = elem->getElementChildren();
+	for (XMLNode::XMLSet<XMLElement*>::iterator it = set.begin();
+	     it != set.end();
+	     it++) {
+		XMLElement* e = *it;
+
+		if (e->matches("name")) {
+			colNames.push_back(e->getFirstText());
+		} else {
+			msg(MSG_FATAL, "Unknown IpfixDbWriterOracle config statement %s\n", e->getName().c_str());
+			continue;
+		}		
+	}
+
+}
+
+IpfixDbWriterOracleCfg::~IpfixDbWriterOracleCfg()
+{
+}
+
+
+IpfixDbWriter* IpfixDbWriterOracleCfg::createInstance()
+{
+    instance = new IpfixDbWriter(hostname, dbname, user, password, port, observationDomainId, bufferRecords, colNames);
+    return instance;
+}
+
+
+bool IpfixDbWriterOracleCfg::deriveFrom(IpfixDbWriterOracleCfg* old)
+{
+    return false;
+}
+
+#endif /*DB_SUPPORT_ENABLED*/
--- src/modules/ipfix/IpfixDbWriterOracleCfg.hpp
+++ src/modules/ipfix/IpfixDbWriterOracleCfg.hpp
@@ -1,5 +1,5 @@
 /*
- * IPFIX Database Reader/Writer Oracle Connector
+ * IPFIX Database Reader/Writer Oracle Connector Configuration
  * Copyright (C) 2011 Philipp Fehre <philipp.fehre@googlemail.com>
  *
  * This program is free software; you can redistribute it and/or
@@ -17,3 +17,50 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
+
+#ifdef ORACLE_SUPPORT_ENABLED
+
+#ifdef IPFIXDBWRITERORACLECFG_H
+#define IPFIXDBWRITERORACLECFG_H
+
+#include <core/XMLElement.h>
+#include <core/Cfg.h>
+
+#include "modules/ipfix/IpfixDbWriterOracle.hpp"
+
+#include <string>
+
+using namespace std;
+
+
+class IpfixDbWriterOracleCfg
+	: public CfgHelper<IpfixDbWriterOracle, IpfixDbWriterOracleCfg>
+{
+public:
+	friend class ConfigManager;
+	
+	virtual IpfixDbWriterOracleCfg* create(XMLElement* e);
+	virtual ~IpfixDbWriterOracleCfg();
+	
+	virtual IpfixDbWriterOracle* createInstance();
+	virtual bool deriveFrom(IpfixDbWriteOracleCfg* old);
+	
+protected:
+	
+	string hostname; /**< hostname of database host */
+	uint16_t port;	/**< port of database */
+	string dbname; /**< database name */
+	string user;	/**< user name for login to database */
+	string password;	/**< password for login to database */
+	uint16_t bufferRecords;	/**< amount of records to buffer until they are written to database */
+	uint32_t observationDomainId;	/**< default observation domain id (overrides the one received in the records */
+	vector<string> colNames; /**< column names */
+
+	void readColumns(XMLElement* elem);
+	IpfixDbWriterOracleCfg(XMLElement*);
+};
+
+
+#endif /* IPFIXDBWRITERORACLECFG_H */
+
+#endif /* ORACLE_SUPPORT_ENABLED */
