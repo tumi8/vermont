13db1884e9321834d2a6fd6952394941278c766b
Philipp Fehre <philipp.fehre@googlemail.com>
2011-12-05 08:09:29 -0800

    writer cfg setup

--- src/modules/ipfix/IpfixDbWriterMongoCfg.cpp
+++ src/modules/ipfix/IpfixDbWriterMongoCfg.cpp
@@ -1,6 +1,6 @@
 /*
  * Vermont Configuration Subsystem
- * Copyright (C) 2009 Vermont Project
+ * Copyright (C) 2011 Vermont Project
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -33,7 +33,7 @@ IpfixDbWriterMongoCfg* IpfixDbWriterMongoCfg::create(XMLElement* e)
 
 IpfixDbWriterMongoCfg::IpfixDbWriterMongoCfg(XMLElement* elem)
     : CfgHelper<IpfixDbWriter, IpfixDbWriterMongoCfg>(elem, "ipfixDbWriter"),
-      port(0), bufferRecords(30), observationDomainId(0)
+      port(27017), bufferRecords(30), observationDomainId(0)
 {
     if (!elem) return;
 
@@ -47,7 +47,7 @@ IpfixDbWriterMongoCfg::IpfixDbWriterMongoCfg(XMLElement* elem)
 			hostname = e->getFirstText();
 		} else if (e->matches("port")) {
 			port = getInt("port");
-		} else if (e->matches("dbname")) {
+		} else if (e->matches("collection")) {
 			dbname = e->getFirstText();
 		} else if (e->matches("username")) {
 			user = e->getFirstText();
@@ -55,7 +55,7 @@ IpfixDbWriterMongoCfg::IpfixDbWriterMongoCfg(XMLElement* elem)
 			password = e->getFirstText();
 		} else if (e->matches("bufferrecords")) {
 			bufferRecords = getInt("bufferrecords");
-		} else if (e->matches("columns")) {
+		} else if (e->matches("properties")) {
 			readColumns(e);
 		} else if (e->matches("observationDomainId")) {
 			observationDomainId = getInt("observationDomainId");
@@ -66,13 +66,12 @@ IpfixDbWriterMongoCfg::IpfixDbWriterMongoCfg(XMLElement* elem)
 		}
 	}
 	if (hostname=="") THROWEXCEPTION("IpfixDbWriterMongoCfg: host not set in configuration!");
-	if (port==0) THROWEXCEPTION("IpfixDbWriterMongoCfg: port not set in configuration!");
 	if (dbname=="") THROWEXCEPTION("IpfixDbWriterMongoCfg: dbname not set in configuration!");
 	if (user=="") THROWEXCEPTION("IpfixDbWriterMongoCfg: username not set in configuration!");
 }
 
 void IpfixDbWriterMongoCfg::readColumns(XMLElement* elem) {
-	colNames.clear();
+	propNames.clear();
 	XMLNode::XMLSet<XMLElement*> set = elem->getElementChildren();
 	for (XMLNode::XMLSet<XMLElement*>::iterator it = set.begin();
 	     it != set.end();
@@ -80,9 +79,9 @@ void IpfixDbWriterMongoCfg::readColumns(XMLElement* elem) {
 		XMLElement* e = *it;
 
 		if (e->matches("name")) {
-			colNames.push_back(e->getFirstText());
+			propNames.push_back(e->getFirstText());
 		} else {
-			msg(MSG_FATAL, "Unknown IpfixDbWriter config statement %s\n", e->getName().c_str());
+			msg(MSG_FATAL, "Unknown IpfixDbWriterMongo config statement %s\n", e->getName().c_str());
 			continue;
 		}		
 	}
@@ -96,14 +95,16 @@ IpfixDbWriterMongoCfg::~IpfixDbWriterMongoCfg()
 
 IpfixDbWriter* IpfixDbWriterMongoCfg::createInstance()
 {
-    instance = new IpfixDbWriter(hostname, dbname, user, password, port, observationDomainId, bufferRecords, colNames);
-    return instance;
+  instance = new IpfixDbWriterMongo(hostname, collection, user, password, port, observationDomainId, bufferRecords, propNames);
+	msg(MSG_DEBUG, "IpfixDbWriterMongo configuration host %s collection %s user %s password %s port %i observationDomainId %i bufferRecords %i\n", 
+	  hostname.c_str(), collection.c_str(), user.c_str(), password.c_str(), port, observationDomainId, bufferRecords);
+  return instance;
 }
 
 
 bool IpfixDbWriterMongoCfg::deriveFrom(IpfixDbWriterMongoCfg* old)
 {
-    return false;
+  return false;
 }
 
 #endif /*MONGO_SUPPORT_ENABLED*/
