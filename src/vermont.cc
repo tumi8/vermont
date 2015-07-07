c4f77593073f3e240bffe6f8650b4638ad4fbab3
Luca Boccassi <lboccass@brocade.com>
2015-07-07 13:15:34 +0100

    Add CLI options to daemonize
    Add a number of new CLI options, which enable Vermont to run as an
    old-style *nix daemon. These include forking itself in the
    background, changing user and group id, and creating a PID file.
    Useful when being run by a SysV Init script.
    
    New help text:
    
    VERsatile MONitoring Tool - VERMONT
     MANDATORY OPTIONS:
     -f, --config-file FILE     Use configuration file
    
     OTHER OPTIONS:
     -h, --help                 Display this help and exit
     -d, --debug                Log verbosity: -d NOTICE, -dd INFO,
                                    -ddd DEBUG
     -b, --daemon               Run in daemon mode (implies -q)
     -p, --pid-file FILE        Set process id filename (use with -d)
     -u, --user USER            Change user to USER (use with -d)
     -g, --group GROUP          Change group to GROUP (use with -d)

--- src/vermont.cc
+++ src/vermont.cc
@@ -31,6 +31,10 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
+#include <getopt.h>
+#include <pwd.h>
+#include <grp.h>
+#include <syslog.h>
 
 /* own systems */
 #include "common/msg.h"
@@ -42,8 +46,183 @@
 
 using namespace std;
 
-static void usage();
+struct parameters {
+	const char *config_file;
+	const char *pid_file;
+	uid_t uid;
+	gid_t gid;
+	int log_level;
+	bool daemon_mode;
+};
 
+
+static void
+record_pid (const char *pidfile)
+{
+	FILE *f = fopen(pidfile, "w");
+	if (!f) {
+		fprintf(stderr, "Could not open pid-file %s for writing", pidfile);
+		exit(EXIT_FAILURE);
+	} else {
+		fprintf(f, "%d\n", getpid());
+		fclose(f);
+	}
+}
+
+static void
+set_groups (uid_t uid, gid_t gid)
+{
+	int groups_count = 0;
+	gid_t *groups = NULL;
+
+	struct passwd *pw = getpwuid(uid);
+	if (!pw) {
+		fprintf(stderr, "could not getpwuid: %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * With groups NULL and groups_count 0, getgrouplist will write the actual
+	 * number of groups into groups_count.
+	 */
+	getgrouplist(pw->pw_name, gid, groups, &groups_count);
+
+	/*
+	 * List must be on the stack.
+	 */
+	groups = (gid_t *)alloca(groups_count * sizeof(gid_t));
+
+	if (getgrouplist(pw->pw_name, gid, groups, &groups_count) <= 0) {
+		fprintf(stderr, "could not getgrouplist: %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	if (setgroups(groups_count, groups) < 0) {
+		fprintf(stderr, "could not setgroups: %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void
+daemonise (const char *pid_file, uid_t uid, gid_t gid)
+{
+	/*
+	 * 1 to stay in the current working dir, 0 to redirect stdin/out/err to
+	 * dev/null
+	 */
+	if (daemon(1, 0) < 0) {
+		fprintf(stderr, "daemon failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid_file) {
+		record_pid(pid_file);
+	}
+
+	if (gid) {
+		set_groups(uid, gid);
+	}
+
+	if (uid && setreuid(0, uid) < 0) {
+		fprintf(stderr, "setreuid %u fail: %s", uid, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void
+usage (int status)
+{
+	printf("VERsatile MONitoring Tool - VERMONT\n"
+			" MANDATORY OPTIONS:\n"
+			" -f, --config-file FILE     Use configuration file\n"
+			"\n OTHER OPTIONS:\n"
+			" -h, --help                 Display this help and exit\n"
+			" -d, --debug                Log verbosity: -d NOTICE, -dd INFO,\n"
+			"                                -ddd DEBUG\n"
+			" -b, --daemon               Run in daemon mode (implies -q)\n"
+			" -p, --pid-file FILE        Set process id filename (use with -d)\n"
+			" -u, --user USER            Change user to USER (use with -d)\n"
+			" -g, --group GROUP          Change group to GROUP (use with -d)\n"
+	);
+
+	exit(status);
+}
+
+static int
+parse_args (int argc, char **argv, struct parameters *params)
+{
+	int opt, ret, option_index;
+	struct passwd *pw;
+	struct group *gr;
+
+	static const struct option long_opts[] = {
+			{ "help",         no_argument,       NULL, 'h' },
+			{ "config-file",  required_argument, NULL, 'f' },
+			{ "daemon",       no_argument,       NULL, 'b' },
+			{ "pid-file",     required_argument, NULL, 'p' },
+			{ "user",         required_argument, NULL, 'u' },
+			{ "group",        required_argument, NULL, 'g' },
+			{ "debug",        no_argument,       NULL, 'd' },
+			{ NULL, 0, NULL, 0}
+	};
+
+	while ((opt = getopt_long(argc, argv, "hbp:u:g:df:", long_opts,
+			&option_index)) != EOF) {
+		switch (opt) {
+		case 'b':
+			params->daemon_mode = true;
+			break;
+
+		case 'f':
+			params->config_file = optarg;
+			break;
+
+		case 'p':
+			params->pid_file = optarg;
+			break;
+
+		case 'u':
+			// Drop privileges and run as this user after initialisation.
+			pw = getpwnam(optarg);
+			if (!pw) {
+				fprintf(stderr, "unknown user '%s'\n", optarg);
+				exit(EXIT_FAILURE);
+			}
+			params->uid = pw->pw_uid;
+			break;
+
+		case 'g':
+			// drop privileges and run as this group after initialisation.
+			gr = getgrnam(optarg);
+			if (!gr) {
+				fprintf(stderr, "unknown group '%s'\n", optarg);
+				exit(EXIT_FAILURE);
+			}
+			params->gid = gr->gr_gid;
+			break;
+
+		case 'h':
+			usage(EXIT_SUCCESS);
+			break;
+
+		case 'd':
+			params->log_level++;
+			break;
+
+		default:
+			usage(EXIT_FAILURE);
+			break;
+		}
+	}
+
+	/*
+	 * reset getopt library
+	 */
+	ret = optind - 1;
+	optind = 0;
+
+	return ret;
+}
 
 int main(int ac, char **dc)
 {
@@ -62,40 +241,28 @@ int main(int ac, char **dc)
 	 * the latter. */
 	ConfigManager manager;
 	string statFile = "stats.log";
+	struct parameters parameters;
 
-	int c, debug_level=MSG_ERROR;
-	char *config_file=NULL;
+	memset(&parameters, 0, sizeof(struct parameters));
+	parameters.log_level = MSG_ERROR;
+
+	/* parse command line */
+	if (parse_args (ac, dc, &parameters) < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	if (parameters.config_file == NULL) {
+		msg(MSG_FATAL, "no config file given, but mandatory");
+		usage(EXIT_FAILURE);
+	}
+
+	if (parameters.daemon_mode) {
+		daemonise(parameters.pid_file, parameters.uid, parameters.gid);
+	}
 
 	msg_init();
 	/**< Wrapper for the main thread's signal handlers*/
 	MainSignalHandler main_signal_handler;
-
-	/* parse command line */
-	while ((c=getopt(ac, dc, "hf:d")) != -1) {
-
-		switch (c) {
-
-		case 'f':
-			config_file=optarg;
-			break;
-
-		case 'd':
-			debug_level++;
-			break;
-
-		case 'h':
-		default:
-			/* print usage and quit vermont, if unknow switch */
-			usage();
-			exit(1);
-		}
-	}
-
-	if (config_file == NULL) {
-		msg(MSG_FATAL, "no config file given, but mandatory");
-		usage();
-		return -1;
-	}
 
 #ifdef __APPLE__
 	if (semaphore_create(mach_task_self(), &mainSemaphore, SYNC_POLICY_FIFO, 0) != KERN_SUCCESS) {
@@ -106,11 +273,11 @@ int main(int ac, char **dc)
 	}
 
 	/* setup verboseness */
-	msg(MSG_DIALOG, "message debug level is %d", debug_level);
-	msg_setlevel(debug_level);
+	msg(MSG_DIALOG, "message debug level is %d", parameters.log_level);
+	msg_setlevel(parameters.log_level);
 
 
-	manager.parseConfig(string(config_file));
+	manager.parseConfig(string(parameters.config_file));
 
 	sigset_t sigmask;
 	sigemptyset(&sigmask);
@@ -128,28 +295,13 @@ int main(int ac, char **dc)
 
 		if (reload_config) {
 			msg(MSG_INFO, "reconfiguring vermont");
-			manager.parseConfig(string(config_file));
+			manager.parseConfig(string(parameters.config_file));
 			reload_config = false;
 		}
 	}
+	msg(MSG_FATAL, "got signal - exiting");
 	manager.shutdown();
 }
-
-
-/* bla bla bla */
-static void usage()
-{
-	printf(
-		"VERsatile MONitoring Tool - VERMONT\n" \
-		" mandatory:\n" \
-		"    -f <xmlfile>     load config\n" \
-		" optional:\n" \
-		"    -d               increase debug level (specify multiple for higher debug levels)\n"
-	);
-}
-
-
-
 
 //static void __cplusplus_really_sucks_andPeopleUsingVeryStrangeNamingConventionsWithLongLongExplicitBlaBlaAndfUnNycasE()
 //{
