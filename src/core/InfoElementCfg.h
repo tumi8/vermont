502036c2322b96322e8a793efd031450aa1c0e06
Luca Boccassi <lboccass@brocade.com>
2015-08-13 17:13:35 +0100

    Add operator== to InfoElementCfg

--- src/core/InfoElementCfg.h
+++ src/core/InfoElementCfg.h
@@ -86,6 +86,17 @@ public:
 
 	bool getAutoAddV4PrefixLength() { return autoAddV4PrefixLength; }
 
+	bool operator==(const InfoElementCfg &other) const {
+		if (other.ieLength != ieLength) return false;
+		if (other.ieId != ieId) return true;
+		if (other.enterpriseNumber != enterpriseNumber) return false;
+		if (other.ieName != ieName) return false;
+		if (other.modifier != modifier) return false;
+		if (other.match != match) return false;
+		if (other.autoAddV4PrefixLength != autoAddV4PrefixLength) return false;
+		return true;
+	}
+
 private:
 	std::string ieName;
 	uint16_t ieLength;
