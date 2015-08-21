d56787cf39916eb04864a72e18eb647253313960
Luca Boccassi <lboccass@brocade.com>
2015-08-21 17:11:28 +0100

    Fix error message at exit
    There were spurious error messages at exit, when de-initialising
    templates that are not there, the helper functions return -1, but it
    is not actually an error.
    
    19/08 18:11:17 ERROR  : failed to deinitialize template 2
    19/08 18:11:17 ERROR  : failed to deinitialize template 3
    19/08 18:11:17 ERROR  : failed to deinitialize template 4
    19/08 18:11:17 ERROR  : failed to deinitialize template 5
    19/08 18:11:17 ERROR  : failed to deinitialize template 6
    19/08 18:11:17 ERROR  : failed to deinitialize template 7
    19/08 18:11:17 ERROR  : failed to deinitialize template 8
    19/08 18:11:17 ERROR  : failed to deinitialize template 9
    19/08 18:11:17 ERROR  : failed to deinitialize template 10
    19/08 18:11:17 ERROR  : failed to deinitialize template 11
    19/08 18:11:17 ERROR  : failed to deinitialize template 12
    19/08 18:11:17 ERROR  : failed to deinitialize template 13
    19/08 18:11:17 ERROR  : failed to deinitialize template 14
    19/08 18:11:17 ERROR  : failed to deinitialize template 15

--- src/common/ipfixlolib/ipfixlolib.c
+++ src/common/ipfixlolib/ipfixlolib.c
@@ -1923,8 +1923,8 @@ static int ipfix_deinit_template_array(ipfix_exporter *exporter)
         
 	for(i=0; i< exporter->ipfix_lo_template_maxsize; i++) {
                 // try to free all templates:
-	    if (ipfix_deinit_template(&(exporter->template_arr[i]) )) {
-                msg(MSG_ERROR, "failed to deinitialize template %i", i);
+	    if (!ipfix_deinit_template(&(exporter->template_arr[i]) )) {
+                msg(MSG_DEBUG, "Deinitialized template %i", i);
 	    }
         }
         free(exporter->template_arr);
